c1975abe79427cf5e189275fc74519f23e586f54
Roy Murphy <roycoins@gmail.com>
2025-11-13 17:57:22 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // src/main.rs
-use zeta::{compile_and_run_zeta, parse_zeta};
+use zeta::compile_and_run_zeta;
 
 fn main() {
     let code = r#"
@@ -11,8 +11,13 @@ impl Addable for i32 {
     fn add(self: i32, rhs: i32) -> i32;
 }
 
-fn use_add<T>(a: T, b: T) -> i32 where T: Addable {
-    a.add(b);
+impl Addable<i32> for Vec<i32> {
+    fn add(self: Vec<i32>, rhs: i32) -> Vec<i32>;
+}
+
+fn use_vec_add(v: Vec<i32>, x: i32) -> i32 where Vec<i32>: Addable<i32> {
+    let res = v.add(x);
+    42  // Placeholder return
 }
 "#;
     match compile_and_run_zeta(code) {
