a986fec8dc252f0b1035b45a77174eda81667bbf
Roy Murphy <roycoins@gmail.com>
2025-11-13 17:50:06 +0000

    Create ast.rs

---
+++ src/ast.rs
@@ -0,0 +1,13 @@
+// src/ast.rs
+#[derive(Debug, Clone)]
+pub enum AstNode {
+    ConceptDef { name: String, params: Vec<String>, methods: Vec<AstNode> },
+    Method { name: String, params: Vec<(String, String)>, ret: String },
+    ImplBlock { concept: String, ty: String, body: Vec<AstNode> },
+    FuncDef { name: String, generics: Vec<String>, params: Vec<(String, String)>, ret: String, body: Vec<AstNode>, where_clause: Option<Vec<(String, String)>> },
+    Call { method: String, receiver: String, args: Vec<String> },
+    Lit(i64),
+    Var(String),
+    Borrow(String),
+    Assign(String, Box<AstNode>),
+}
