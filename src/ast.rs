44f54dd487c149fea62847bfce3170e7bb966cec
Roy Murphy <roycoins@gmail.com>
2025-11-13 18:54:35 +0000

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -4,13 +4,13 @@ pub enum AstNode {
     ConceptDef { name: String, params: Vec<String>, methods: Vec<AstNode> },
     Method { name: String, params: Vec<(String, String)>, ret: String },
     ImplBlock { concept: String, ty: String, body: Vec<AstNode> },
-    FuncDef { name: String, generics: Vec<String>, params: Vec<(String, String)>, ret: String, body: Vec<AstNode>, where_clause: Option<Vec<(String, String)>> },
+    FuncDef { name: String, generics: Vec<String>, params: Vec<(String, String)>, ret: String, body: Vec<AstNode>, where_clause: Option<Vec<(String, String)>>, attrs: Vec<String> },
     Call { method: String, receiver: String, args: Vec<String> },
     Lit(i64),
     Var(String),
     Borrow(String),
     Assign(String, Box<AstNode>),
     Defer(Box<AstNode>),
-    ActorDef { name: String, methods: Vec<AstNode> }, // actor Name { async fn handle(msg: Msg) }
+    ActorDef { name: String, methods: Vec<AstNode> },
     SpawnActor { actor_ty: String, init_args: Vec<String> },
 }
