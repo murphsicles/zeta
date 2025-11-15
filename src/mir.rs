d59d8aeaf51d200e0c2fbce62d8df424bf68267d
Roy Murphy <roycoins@gmail.com>
2025-11-15 09:07:50 +0000

    Create mir.rs

---
+++ src/mir.rs
@@ -0,0 +1,98 @@
+// src/mir.rs
+use crate::ast::AstNode;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Mir {
+    pub stmts: Vec<MirStmt>,
+    pub locals: HashMap<String, u32>,
+}
+
+#[derive(Debug, Clone)]
+pub enum MirStmt {
+    Assign { lhs: u32, rhs: MirExpr },
+    Call { func: String, args: Vec<u32> },
+    Borrow { var: u32 },
+    Return { val: u32 },
+    Defer { stmt: Box<MirStmt> },
+}
+
+#[derive(Debug, Clone)]
+pub enum MirExpr {
+    Var(u32),
+    Lit(i64),
+    MethodCall { recv: u32, method: String, args: Vec<u32> },
+}
+
+pub struct MirGen {
+    next_id: u32,
+    locals: HashMap<String, u32>,
+}
+
+impl MirGen {
+    pub fn new() -> Self {
+        Self { next_id: 0, locals: HashMap::new() }
+    }
+
+    pub fn gen_mir(&mut self, ast: &AstNode) -> Mir {
+        let mut stmts = vec![];
+        match ast {
+            AstNode::FuncDef { body, params, .. } => {
+                for (pname, _) in params {
+                    let id = self.next_id;
+                    self.locals.insert(pname.clone(), id);
+                    self.next_id += 1;
+                }
+                for node in body {
+                    if let Some(stmt) = self.gen_stmt(node) {
+                        stmts.push(stmt);
+                    }
+                }
+                stmts.push(MirStmt::Return { val: 0 }); // Stub
+            }
+            _ => {}
+        }
+        Mir { stmts, locals: self.locals.clone() }
+    }
+
+    fn gen_stmt(&mut self, node: &AstNode) -> Option<MirStmt> {
+        match node {
+            AstNode::Assign(name, expr) => {
+                let lhs = self.fresh_id();
+                self.locals.insert(name.clone(), lhs);
+                let rhs = self.gen_expr(expr)?;
+                Some(MirStmt::Assign { lhs, rhs })
+            }
+            AstNode::Call { receiver, method, args } => {
+                let recv_id = self.locals.get(receiver).copied()?;
+                let arg_ids: Vec<u32> = args.iter().map(|a| self.locals.get(a).copied().unwrap_or(0)).collect();
+                Some(MirStmt::Call { func: format!("{}.{}", receiver, method), args: arg_ids })
+            }
+            AstNode::Borrow(var) => {
+                let vid = self.locals.get(var).copied()?;
+                Some(MirStmt::Borrow { var: vid })
+            }
+            AstNode::Defer(inner) => self.gen_stmt(inner).map(|s| MirStmt::Defer(Box::new(s))),
+            _ => None,
+        }
+    }
+
+    fn gen_expr(&mut self, node: &AstNode) -> Option<MirExpr> {
+        match node {
+            AstNode::Lit(n) => Some(MirExpr::Lit(*n)),
+            AstNode::Var(v) => self.locals.get(v).map(|id| MirExpr::Var(*id)),
+            AstNode::Call { receiver, method, args } => {
+                let recv_id = self.locals.get(receiver).copied()?;
+                let arg_ids: Vec<u32> = args.iter().map(|a| self.locals.get(a).copied().unwrap_or(0)).collect();
+                Some(MirExpr::MethodCall { recv: recv_id, method: method.clone(), args: arg_ids })
+            }
+            _ => None,
+        }
+    }
+
+    fn fresh_id(&mut self) -> u32 {
+        let id = self.next_id;
+        self.next_id += 1;
+        id
+    }
+}
