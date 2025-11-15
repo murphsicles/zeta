236a70219435957c9cd88df294b0c6db2fd00389
Roy Murphy <roycoins@gmail.com>
2025-11-15 10:46:00 +0000

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use crate::{compile_and_run_zeta, parse_zeta, Resolver};
     use crate::ast::AstNode;
     use crate::mir::MirGen;
     use crate::resolver::MonoKey;
+    use crate::{Resolver, compile_and_run_zeta, parse_zeta};
 
     #[test]
     fn test_parse_addable() {
@@ -28,7 +28,9 @@ impl Send for i32 {}
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         assert!(res.typecheck(&asts));
     }
 
@@ -48,7 +50,9 @@ fn test_phantom<T>() -> T { TimingOwned<i32> 0 }
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         assert!(res.typecheck(&asts));
     }
 
@@ -57,7 +61,9 @@ fn test_phantom<T>() -> T { TimingOwned<i32> 0 }
         let input = r#"#[derive(Copy)] struct Foo(i32);"#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         assert!(res.resolve_impl("Copy", "Foo").is_some());
     }
 
@@ -71,7 +77,9 @@ fn test() -> i32 {
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         let ast_hash = format!("{:?}", asts[0]);
         let mir = res.get_cached_mir(&ast_hash).unwrap();
         assert!(!mir.stmts.is_empty());
@@ -87,7 +95,9 @@ impl B for i32 {}
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         assert!(res.typecheck(&asts));
     }
 
@@ -103,7 +113,9 @@ fn semiring_test() -> i32 {
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         let ast_hash = format!("{:?}", asts[0]);
         let mir = res.get_cached_mir(&ast_hash).unwrap();
         assert!(mir.ctfe_consts.contains_key(&2));
@@ -117,7 +129,9 @@ fn generic_add<T>(a: T, b: T) -> T { a.add(b) }
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         let key = MonoKey(("generic_add".to_string(), vec!["i32".to_string()]));
         let mono_ast = res.monomorphize(key.clone(), &asts[0]);
         assert_eq!(mono_ast.generics, vec![]);
@@ -137,7 +151,44 @@ impl CacheSafe for SafeChannel {}
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
-        for ast in &asts { res.register(ast.clone()); }
+        for ast in &asts {
+            res.register(ast.clone());
+        }
         assert!(res.typecheck(&asts)); // Validates CacheSafe
+    }
+
+    #[test]
+    fn test_affine_borrow() {
+        let input = r#"
+fn affine_test() -> i32 {
+    let x = 42;
+    let y = x; // Move: affine consume
+    y // Use post-move: should fail
+}
+"#;
+        let (_, asts) = parse_zeta(input).unwrap();
+        let mut res = Resolver::new();
+        for ast in &asts {
+            res.register(ast.clone());
+        }
+        assert!(!res.typecheck(&asts)); // Affine violation
+    }
+
+    #[test]
+    fn test_affine_ok() {
+        let input = r#"
+fn affine_ok() -> i32 {
+    let x = 42;
+    let y = x; // Move
+    // No use of x after
+    y
+}
+"#;
+        let (_, asts) = parse_zeta(input).unwrap();
+        let mut res = Resolver::new();
+        for ast in &asts {
+            res.register(ast.clone());
+        }
+        assert!(res.typecheck(&asts)); // Affine ok
     }
 }
