733953eb2ab602e72f2982c1d405eb9ad72facc9
Roy Murphy <roycoins@gmail.com>
2025-11-15 09:15:35 +0000

    Update tests.rs

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,8 @@
-// src/tests.rs
 #[cfg(test)]
 mod tests {
     use crate::{compile_and_run_zeta, parse_zeta, Resolver};
     use crate::ast::AstNode;
+    use crate::mir::MirGen;
 
     #[test]
     fn test_parse_addable() {
@@ -37,30 +37,57 @@ impl Send for i32 {}
 fn simple() -> i32 { 42 }
 "#;
         let res = compile_and_run_zeta(input).unwrap();
-        assert_eq!(res, 0); // Stub return 0
+        assert_eq!(res, 0);
     }
 
     #[test]
     fn test_ergonomics_infer() {
         let input = r#"
-fn test_phantom<T>() -> T { /* stub */ TimingOwned<i32> 0 }
+fn test_phantom<T>() -> T { TimingOwned<i32> 0 }
 "#;
         let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
         for ast in &asts { res.register(ast.clone()); }
-        assert!(res.typecheck(&asts)); // Infer <()>
+        assert!(res.typecheck(&asts));
     }
 
     #[test]
     fn test_derive_copy() {
-        let input = r#"
-#[derive(Copy)]
-struct Foo(i32);
-"#;
-        // Parser stub for struct, but test resolve
-        let (_, asts) = parse_zeta(input).unwrap(); // Assume parses as Derive
+        let input = r#"#[derive(Copy)] struct Foo(i32);"#;
+        let (_, asts) = parse_zeta(input).unwrap();
         let mut res = Resolver::new();
         for ast in &asts { res.register(ast.clone()); }
         assert!(res.resolve_impl("Copy", "Foo").is_some());
+    }
+
+    #[test]
+    fn test_mir_gen() {
+        let input = r#"
+fn test() -> i32 {
+    let x = 42;
+    x
+}
+"#;
+        let (_, asts) = parse_zeta(input).unwrap();
+        let mut res = Resolver::new();
+        for ast in &asts { res.register(ast.clone()); }
+        let ast_hash = format!("{:?}", asts[0]);
+        let mir = res.get_cached_mir(&ast_hash).unwrap();
+        assert!(!mir.stmts.is_empty());
+    }
+
+    #[test]
+    fn test_parallel_resolve() {
+        let input = r#"
+concept A {}
+concept B {}
+impl A for i32 {}
+impl B for i32 {}
+"#;
+        let (_, asts) = parse_zeta(input).unwrap();
+        let mut res = Resolver::new();
+        for ast in &asts { res.register(ast.clone()); }
+        // Parallel typecheck should pass
+        assert!(res.typecheck(&asts));
     }
 }
