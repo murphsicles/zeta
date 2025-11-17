8df0dea9f862facd363c6d3d2f632ccc42e545cb
Roy Murphy <roycoins@gmail.com>
2025-11-17 06:53:02 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ pub mod borrow;
 pub mod codegen;
 pub mod mir;
 pub mod parser;
-pub mod resolver;
 pub mod plan;
+pub mod resolver;
 pub mod xai;
 
 pub use ast::AstNode;
 pub use borrow::BorrowChecker;
-pub use codegen::{compile_and_run_zeta, LLVMCodegen};
+pub use codegen::{LLVMCodegen, compile_and_run_zeta};
 pub use mir::{Mir, MirGen};
 pub use parser::parse_zeta;
 pub use resolver::Resolver;
