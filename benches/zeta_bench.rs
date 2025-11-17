2c84a4d9fef7d69954d171cf16d6012f7caabb1e
Roy Murphy <roycoins@gmail.com>
2025-11-17 06:45:02 +0000

    Update zeta_bench.rs

--- benches/zeta_bench.rs
+++ benches/zeta_bench.rs
@@ -1,5 +1,5 @@
 // benches/zeta_bench.rs: Criterion benchmarks for Zeta (semiring/add, actors, EOP algos)
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use zeta::{compile_and_run_zeta, parse_zeta};
 
 fn bench_semiring_add(c: &mut Criterion) {
@@ -12,9 +12,11 @@ fn bench_add() -> i32 {
     sum
 }
 "#;
-    c.bench_function("zeta_semiring_add", |b| b.iter(|| {
-        let _ = black_box(compile_and_run_zeta(black_box(input)).unwrap());
-    }));
+    c.bench_function("zeta_semiring_add", |b| {
+        b.iter(|| {
+            let _ = black_box(compile_and_run_zeta(black_box(input)).unwrap());
+        })
+    });
 }
 
 fn bench_eop_matrix(c: &mut Criterion) {
@@ -23,9 +25,11 @@ concept Semiring { fn add(self: Self, rhs: Self) -> Self; fn mul(self: Self, rhs
 impl Semiring for i32 { fn add(self: i32, rhs: i32) -> i32 { self + rhs } fn mul(self: i32, rhs: i32) -> i32 { self * rhs } }
 fn matrix_mul() -> i32 { 1 } // Stub EOP
 "#;
-    c.bench_function("zeta_eop_matrix", |b| b.iter(|| {
-        let _ = black_box(compile_and_run_zeta(black_box(input)).unwrap());
-    }));
+    c.bench_function("zeta_eop_matrix", |b| {
+        b.iter(|| {
+            let _ = black_box(compile_and_run_zeta(black_box(input)).unwrap());
+        })
+    });
 }
 
 criterion_group!(benches, bench_semiring_add, bench_eop_matrix);
