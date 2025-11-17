83feb41b504ee3b5271179be962777f71b9348a9
Roy Murphy <roycoins@gmail.com>
2025-11-17 06:46:22 +0000

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,18 +1,57 @@
 #[derive(Debug, Clone)]
 pub enum AstNode {
-    ConceptDef { name: String, params: Vec<String>, methods: Vec<AstNode> },
-    Method { name: String, params: Vec<(String, String)>, ret: String },
-    ImplBlock { concept: String, ty: String, body: Vec<AstNode> },
-    FuncDef { name: String, generics: Vec<String>, params: Vec<(String, String)>, ret: String, body: Vec<AstNode>, where_clause: Option<Vec<(String, String)>>, attrs: Vec<String> },
-    Call { method: String, receiver: String, args: Vec<String> },
+    ConceptDef {
+        name: String,
+        params: Vec<String>,
+        methods: Vec<AstNode>,
+    },
+    Method {
+        name: String,
+        params: Vec<(String, String)>,
+        ret: String,
+    },
+    ImplBlock {
+        concept: String,
+        ty: String,
+        body: Vec<AstNode>,
+    },
+    FuncDef {
+        name: String,
+        generics: Vec<String>,
+        params: Vec<(String, String)>,
+        ret: String,
+        body: Vec<AstNode>,
+        where_clause: Option<Vec<(String, String)>>,
+        attrs: Vec<String>,
+    },
+    Call {
+        method: String,
+        receiver: String,
+        args: Vec<String>,
+    },
     Lit(i64),
     Var(String),
     Borrow(String),
     Assign(String, Box<AstNode>),
     Defer(Box<AstNode>),
-    ActorDef { name: String, methods: Vec<AstNode> },
-    SpawnActor { actor_ty: String, init_args: Vec<String> },
-    TimingOwned { ty: String, inner: Box<AstNode> },
-    Derive { ty: String, traits: Vec<String> },
-    StructDef { name: String, fields: Vec<(String, String)> }, // For structural traits
+    ActorDef {
+        name: String,
+        methods: Vec<AstNode>,
+    },
+    SpawnActor {
+        actor_ty: String,
+        init_args: Vec<String>,
+    },
+    TimingOwned {
+        ty: String,
+        inner: Box<AstNode>,
+    },
+    Derive {
+        ty: String,
+        traits: Vec<String>,
+    },
+    StructDef {
+        name: String,
+        fields: Vec<(String, String)>,
+    }, // For structural traits
 }
