3a244d47f3ce5c3811bbe28a552ef89cb3569460
Roy Murphy <roycoins@gmail.com>
2025-11-20 03:43:46 +0000

    Update resolver.rs

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,15 +1,8 @@
 // src/resolver.rs
 use crate::ast::AstNode;
 use crate::borrow::{BorrowChecker, BorrowState};
-use crate::mir::Mir;
 use std::collections::{HashMap, HashSet};
 use std::sync::{Arc, Mutex};
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct MonoKey {
-    pub func: String,
-    pub types: Vec<String>,
-}
 
 #[derive(Debug)]
 pub struct Resolver {
@@ -39,13 +32,34 @@ impl Resolver {
 
     pub fn register(&mut self, ast: AstNode) {
         match ast {
-            AstNode::ConceptDef { name, .. } => { self.concepts.insert(name, ast); }
-            AstNode::ImplBlock { concept, ty, .. } => { self.impls.insert((concept, ty), ast); }
-            AstNode::FuncDef { name, .. } => { self.funcs.insert(name, ast); }
-            AstNode::StructDef { name, .. } => { self.structs.insert(name, ast); }
+            AstNode::ConceptDef { name, .. } => {
+                self.concepts.insert(name, ast);
+            }
+            AstNode::ImplBlock { concept, ty, .. } => {
+                self.impls.insert((concept, ty), ast);
+            }
+            AstNode::FuncDef { name, .. } => {
+                self.funcs.insert(name, ast);
+            }
+            AstNode::StructDef { name, .. } => {
+                self.structs.insert(name, ast);
+            }
+            AstNode::Derive { ty, traits } => {
+                for tr in traits {
+                    self.impls.insert(
+                        (tr.clone(), ty.clone()),
+                        AstNode::ImplBlock {
+                            concept: tr,
+                            ty: ty.clone(),
+                            body: vec![],
+                        },
+                    );
+                }
+            }
             _ => {}
         }
     }
+
     pub fn resolve_impl(&self, concept: &str, ty: &str) -> Option<Arc<AstNode>> {
         let key = (concept.to_string(), ty.to_string());
         {
@@ -60,19 +74,27 @@ impl Resolver {
     }
 
     pub fn has_method(&self, concept: &str, ty: &str, method: &str) -> bool {
-        self.resolve_impl(concept, ty).map_or(false, |imp| {
-            if let AstNode::ImplBlock { body, .. } = imp.as_ref() {
-                body.iter().any(|m| matches!(m, AstNode::Method { name, .. } if name == method))
-            } else {
-                false
-            }
-        })
+        self.resolve_impl(concept, ty)
+            .map_or(false, |imp| {
+                if let AstNode::ImplBlock { body, .. } = imp.as_ref() {
+                    body.iter().any(|m| {
+                        matches!(m, AstNode::Method { name, .. } if name == method)
+                    })
+                } else {
+                    false
+                }
+            })
     }
 
     pub fn typecheck(&self, asts: &[AstNode]) -> bool {
         asts.iter().all(|ast| {
             match ast {
-                AstNode::FuncDef { params, body, attrs, .. } => {
+                AstNode::FuncDef {
+                    params,
+                    body,
+                    attrs,
+                    ..
+                } => {
                     let stable_abi_ok = !attrs.contains(&"stable_abi".to_string())
                         || !params.iter().any(|(_, t)| t.contains('<'));
                     let mut bc = BorrowChecker::new();
@@ -85,6 +107,10 @@ impl Resolver {
                     stable_abi_ok && borrow_ok
                 }
                 AstNode::TimingOwned { ty, .. } => self.resolve_impl("CacheSafe", ty).is_some(),
+                AstNode::SpawnActor { actor_ty, .. } => {
+                    self.resolve_impl("Send", actor_ty).is_some()
+                        && self.resolve_impl("CacheSafe", actor_ty).is_some()
+                }
                 _ => true,
             }
         })
