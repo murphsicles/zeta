92d65590690fb48564f489a6c178619a8929c76d
Roy Murphy <roycoins@gmail.com>
2025-11-21 08:16:44 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,35 @@
 // src/main.rs
 use zeta::{parse_zeta, Resolver, LLVMCodegen};
 use inkwell::context::Context;
-use std::error::Error;
+use std::fs;
 
-pub fn compile_and_run_zeta(input: &str) -> Result<i64, Box<dyn Error>> {
-    let (_, asts) = parse_zeta(input).map_err(|e| format!("Parse error: {:?}", e))?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let code = fs::read_to_string("examples/selfhost.zeta")?;
+    let (_, asts) = parse_zeta(&code).map_err(|e| format!("Parse error: {:?}", e))?;
+
     let mut resolver = Resolver::new();
-
     for ast in &asts {
         resolver.register(ast.clone());
     }
-
-    if !resolver.typecheck(&asts) {
-        return Err("Typecheck/borrowck failed".into());
-    }
+    resolver.typecheck(&asts);
 
     let context = Context::create();
-    let mut codegen = LLVMCodegen::new(&context, "zeta_module");
+    let mut codegen = LLVMCodegen::new(&context, "selfhost");
 
     let main_func = asts.iter()
         .find(|a| matches!(a, AstNode::FuncDef { name, .. } if name == "main"))
         .ok_or("No main function")?;
 
-    let mut mir = resolver.lower_to_mir(main_func);
-    resolver.fold_semiring_chains(&mut mir);
+    let mir = resolver.lower_to_mir(main_func);
     codegen.gen_mir(&mir);
-
     let ee = codegen.finalize_and_jit()?;
 
-    type MainFn = unsafe extern "C" fn() -> i64;
+    type MainFn = unsafe extern "C" fn() -> i32;
     unsafe {
         let main = ee.get_function::<MainFn>("main").map_err(|_| "No main")?;
-        Ok(main.call())
+        let result = main.call();
+        println!("Self-hosted Zeta result: {}", result);
     }
-}
 
-fn main() {
-    let code = r#"
-fn main() -> i64 {
-    datetime_now()
-}
-"#;
-
-    match compile_and_run_zeta(code) {
-        Ok(res) => println!("Result: {res}"),
-        Err(e) => eprintln!("Error: {e}"),
-    }
+    Ok(())
 }
