4743461966e71deeb981b55cbb38f4e97821010c
Roy Murphy <roycoins@gmail.com>
2025-11-21 08:15:48 +0000

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -29,195 +29,26 @@ fn variable(input: &str) -> IResult<&str, AstNode> {
     map(ident, AstNode::Var).parse(input)
 }
 
-fn parens(input: &str) -> IResult<&str, AstNode> {
-    delimited(ws(tag("(")), expr, ws(tag(")"))).parse(input)
-}
-
-fn fn_call(input: &str) -> IResult<&str, AstNode> {
-    let (i, name) = ident(input)?;
-    let (i, args) = delimited(ws(tag("(")), separated_list0(ws(tag(",")), expr), ws(tag(")")))(i)?;
-    Ok((
-        i,
-        AstNode::Call {
-            receiver: None,
-            method: name,
-            args,
-            type_args: vec![],
-        },
-    ))
-}
-
-fn primary(input: &str) -> IResult<&str, AstNode> {
-    alt((fn_call, literal, variable, parens)).parse(input)
-}
-
-fn method_call(input: &str) -> IResult<&str, AstNode> {
-    let (mut i, mut cur) = primary(input)?;
-    loop {
-        if let Ok((i2, _)) = ws(tag("."))(i) {
-            let (i3, method) = ident(i2)?;
-            let (i4, args) = delimited(ws(tag("(")), separated_list0(ws(tag(",")), expr), ws(tag(")")))(i3)?;
-            cur = AstNode::Call {
-                receiver: Some(Box::new(cur)),
-                method,
-                args,
-                type_args: vec![],
-            };
-            i = i4;
-        } else {
-            break;
-        }
-    }
-    Ok((i, cur))
-}
-
-fn factor(input: &str) -> IResult<&str, AstNode> {
-    let (mut i, mut left) = method_call(input)?;
-    while let Ok((i2, op)) = alt((ws(tag("*")), ws(tag("/")), ws(tag("%"))))(i) {
-        let (i3, right) = method_call(i2)?;
-        left = AstNode::Call {
-            receiver: Some(Box::new(left)),
-            method: match op {
-                "*" => "mul".to_string(),
-                "/" => "div".to_string(),
-                "%" => "rem".to_string(),
-                _ => unreachable!(),
-            },
-            args: vec![right],
-            type_args: vec![],
-        };
-        i = i3;
+fn expr(input: &str) -> IResult<&str, AstNode> {
+    let (i, left) = alt((literal, variable))(input)?;
+    let (i, _) = ws(tag("+"))(i).ok();
+    if let Ok((i, right)) = alt((literal, variable))(i) {
+        return Ok((i, AstNode::Add(Box::new(left), Box::new(right))));
     }
     Ok((i, left))
 }
 
-fn term(input: &str) -> IResult<&str, AstNode> {
-    let (mut i, mut left) = factor(input)?;
-    while let Ok((i2, op)) = alt((ws(tag("+")), ws(tag("-"))))(i) {
-        let (i3, right) = factor(i2)?;
-        left = AstNode::Call {
-            receiver: Some(Box::new(left)),
-            method: if op == "+" { "add" } else { "sub" }.to_string(),
-            args: vec![right],
-            type_args: vec![],
-        };
-        i = i3;
-    }
-    Ok((i, left))
-}
-
-pub fn expr(input: &str) -> IResult<&str, AstNode> {
-    term(input)
-}
-
-/* ---------- statements ---------- */
-
-fn let_stmt(input: &str) -> IResult<&str, AstNode> {
-    preceded(
-        ws(tag("let")),
-        map(
-            tuple((ident, ws(tag("=")), expr, ws(tag(";"))),
-            |(name, _, e, _)| AstNode::Assign(name, Box::new(e)),
-        ),
-    )
-    .parse(input)
-}
-
-fn expr_stmt(input: &str) -> IResult<&str, AstNode> {
-    map(tuple((expr, ws(tag(";")))), |(e, _)| {
-        AstNode::Assign("_".to_string(), Box::new(e))
-    })
-    .parse(input)
-}
-
-fn spawn_expr(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = ws(tag("spawn"))(input)?;
-    let (i, actor_ty) = ident(i)?;
-    let (i, args) = delimited(ws(tag("(")), separated_list0(ws(tag(",")), expr), ws(tag(")")))(i)?;
-    Ok((
-        i,
-        AstNode::SpawnActor {
-            actor_ty,
-            init_args: args.into_iter().map(|_| "tmp".to_string()).collect(),
-        },
-    ))
-}
-
-fn await_expr(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = ws(tag("await"))(input)?;
-    let (i, future) = expr(i)?;
-    Ok((i, AstNode::Await { expr: Box::new(future) }))
-}
-
-fn stmt(input: &str) -> IResult<&str, AstNode> {
-    alt((
-        let_stmt,
-        expr_stmt,
-        map(spawn_expr, |e| AstNode::Assign("_".to_string(), Box::new(e))),
-        map(await_expr, |e| AstNode::Assign("_".to_string(), Box::new(e))),
-    ))
-    .parse(input)
-}
-
-fn block(input: &str) -> IResult<&str, Vec<AstNode>> {
-    delimited(ws(tag("{")), many0(stmt), ws(tag("}")))(input)
-}
-
-/* ---------- top-level definitions ---------- */
-
-fn actor_field(input: &str) -> IResult<&str, (String, String)> {
-    map(tuple((ident, ws(tag(":")), ident)), |(n, _, t)| (n, t)).parse(input)
-}
-
-fn parse_actor(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = ws(tag("actor"))(input)?;
-    let (i, name) = ident(i)?;
-    let (i, state) = delimited(ws(tag("{")), many0(preceded(multispace0, actor_field)), ws(tag("}")))(i)?;
-    Ok((
-        i,
-        AstNode::ActorDef {
-            name,
-            state,
-            methods: vec![],
-        },
-    ))
-}
-
-fn async_fn(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = ws(tag("async"))(input)?;
-    let (i, _) = ws(tag("fn"))(i)?;
-    let (i, name) = ident(i)?;
-    let (i, _) = ws(tag("("))(i)?;
-    let (i, _) = ws(tag(")"))(i)?;
-    let (i, body) = block(i)?;
-    Ok((
-        i,
-        AstNode::AsyncFn {
-            name,
-            params: vec![],
-            ret: "Future<i64>".to_string(),
-            body,
-        },
-    ))
-}
-
-fn ret_ty(input: &str) -> IResult<&str, String> {
-    preceded(ws(tag("->")), ws(ident)).parse(input)
+fn func_body(input: &str) -> IResult<&str, Vec<AstNode>> {
+    delimited(ws(tag("{")), many0(expr), ws(tag("}")))(input)
 }
 
 fn parse_func(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = preceded(ws(tag("fn")), multispace0)(input)?;
+    let (i, _) = ws(tag("fn"))(input)?;
     let (i, name) = ident(i)?;
     let (i, _) = ws(tag("("))(i)?;
     let (i, _) = ws(tag(")"))(i)?;
-    let (i, ret) = opt(ret_ty)(i)?;
-    let (i, body) = block(i)?;
-
-    let ret_expr = body.iter().rev().find_map(|n| match n {
-        AstNode::Assign(s, e) if s == "_" => Some(e.clone()),
-        _ => None,
-    });
-
+    let (i, ret) = opt(preceded(ws(tag("->")), ws(ident)))(i)?;
+    let (i, body) = func_body(i)?;
     Ok((
         i,
         AstNode::FuncDef {
@@ -226,17 +57,12 @@ fn parse_func(input: &str) -> IResult<&str, AstNode> {
             params: vec![],
             ret: ret.unwrap_or_else(|| "i64".to_string()),
             body,
-            where_clause: None,
             attrs: vec![],
-            ret_expr,
+            ret_expr: None,
         },
     ))
 }
 
-fn parse_top(input: &str) -> IResult<&str, AstNode> {
-    alt((parse_actor, async_fn, parse_func)).parse(input)
-}
-
 pub fn parse_zeta(input: &str) -> IResult<&str, Vec<AstNode>> {
-    delimited(multispace0, many0(parse_top), multispace0)(input)
+    delimited(multispace0, many0(parse_func), multispace0)(input)
 }
