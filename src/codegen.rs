911a037627b5d08caa53d7dd4a2dd50e5135696a
Roy Murphy <roycoins@gmail.com>
2025-11-21 01:38:23 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,11 +1,11 @@
 // src/codegen.rs
 use crate::mir::{Mir, MirStmt, SemiringOp};
 use inkwell::context::Context;
-use inkwell::execution_engine::{ExecutionEngine, JitFunction, UnsafeFunctionPointer};
+use inkwell::execution_engine::ExecutionEngine;
 use inkwell::module::Module;
 use inkwell::builder::Builder;
-use inkwell::values::BasicValueEnum;
-use inkwell::types::{IntType, VectorType};
+use inkwell::values::{BasicValueEnum, IntValue, VectorValue};
+use inkwell::types::VectorType;
 use inkwell::OptimizationLevel;
 use std::collections::HashMap;
 use std::error::Error;
@@ -15,7 +15,7 @@ pub struct LLVMCodegen<'ctx> {
     module: Module<'ctx>,
     builder: Builder<'ctx>,
     execution_engine: Option<ExecutionEngine<'ctx>>,
-    i32_type: IntType<'ctx>,
+    i32_type: inkwell::types::IntType<'ctx>,
     i32x4_type: VectorType<'ctx>,
     locals: HashMap<u32, inkwell::values::PointerValue<'ctx>>,
     enable_simd: bool,
@@ -52,7 +52,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                 let value = match rhs {
                     crate::mir::MirExpr::Var(v) => self.load_local(*v),
                     crate::mir::MirExpr::Lit(n) => self.i32_type.const_int(*n as u64, false).into(),
-                    _ => self.i32_type.const_int(0, false).into(),
+                    _ => self.i32_type.const_zero().into(),
                 };
                 let ptr = self.locals.entry(*lhs).or_insert_with(|| {
                     self.builder.build_alloca(self.i32_type, &format!("local_{}", lhs)).unwrap()
@@ -60,7 +60,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                 self.builder.build_store(*ptr, value).unwrap();
             }
             MirStmt::SemiringFold { op, values, result } => {
-                let mut acc = self.load_local(values[0]);
+                let mut acc: BasicValueEnum<'ctx> = self.load_local(values[0]);
 
                 if values.len() >= 4 && self.enable_simd {
                     for chunk in values[1..].chunks(4) {
@@ -71,10 +71,10 @@ impl<'ctx> LLVMCodegen<'ctx> {
                         while elems.len() < 4 {
                             elems.push(self.i32_type.const_zero());
                         }
-                        let vec = VectorType::const_vector(&elems);
+                        let vec = self.i32x4_type.const_vector(&elems);
                         acc = match op {
-                            SemiringOp::Add => self.builder.build_int_add(acc.into_int_value(), vec, "fold_add_simd").unwrap().into(),
-                            SemiringOp::Mul => self.builder.build_int_mul(acc.into_int_value(), vec, "fold_mul_simd").unwrap().into(),
+                            SemiringOp::Add => self.builder.build_int_add(acc.into_vector_value().unwrap(), vec, "fold_add_simd").unwrap().into(),
+                            SemiringOp::Mul => self.builder.build_int_mul(acc.into_vector_value().unwrap(), vec, "fold_mul_simd").unwrap().into(),
                         };
                     }
                 } else {
@@ -91,19 +91,6 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     self.builder.build_alloca(self.i32_type, &format!("result_{}", result)).unwrap()
                 });
                 self.builder.build_store(*ptr, acc).unwrap();
-            }
-            MirStmt::Spawn { actor, dest } => {
-                // stub – full actor runtime in next push
-                let ptr = self.locals.entry(*dest).or_insert_with(|| {
-                    self.builder.build_alloca(self.context.i8_type().ptr_type(inkwell::AddressSpace::Generic), "actor_handle").unwrap()
-                });
-                self.builder.build_store(*ptr, self.load_local(*actor)).unwrap();
-            }
-            MirStmt::Await { future, dest } => {
-                let ptr = self.locals.entry(*dest).or_insert_with(|| {
-                    self.builder.build_alloca(self.i32_type, "await_result").unwrap()
-                });
-                self.builder.build_store(*ptr, self.load_local(*future)).unwrap();
             }
             MirStmt::Return { val } => {
                 let v = self.load_local(*val);
@@ -125,12 +112,5 @@ impl<'ctx> LLVMCodegen<'ctx> {
         let ee = self.module.create_jit_execution_engine(OptimizationLevel::Aggressive)?;
         self.execution_engine = Some(ee.clone());
         Ok(ee)
-    }
-
-    pub fn get_fn<F>(&self, name: &str) -> Option<JitFunction<F>>
-    where
-        F: UnsafeFunctionPointer,
-    {
-        unsafe { self.execution_engine.as_ref()?.get_function(name).ok() }
     }
 }
