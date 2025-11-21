5eeebe4fc59393d509c49a8ff1f4242b9ade9ea8
Roy Murphy <roycoins@gmail.com>
2025-11-21 03:55:32 +0000

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -10,29 +10,34 @@ use nom::{
     IResult,
 };
 
-type PResult<'a, O> = IResult<&'a str, O>;
+type I<'a> = &'a str;
+type Res<'a, O> = IResult<I<'a>, O>;
 
-fn ident(input: &str) -> PResult<String> {
-    let first = alt((alpha1, tag("_")));
-    let rest = many0(alt((alphanumeric1, tag("_"))));
-    map(pair(first, rest), |(f, r): (&str, Vec<&str>)| {
-        let mut s = f.to_string();
-        for p in r {
-            s.push_str(p);
-        }
-        s
-    })(input)
+fn ident(input: I) -> Res<String> {
+    map(
+        pair(
+            alt((alpha1, tag("_"))),
+            many0(alt((alphanumeric1, tag("_")))),
+        ),
+        |(first, rest)| {
+            let mut s = first.to_owned();
+            for p in rest {
+                s.push_str(p);
+            }
+            s
+        },
+    )(input)
 }
 
-fn lit(input: &str) -> PResult<AstNode> {
+fn lit(input: I) -> Res<AstNode> {
     map(i64, AstNode::Lit)(input)
 }
 
-fn var(input: &str) -> PResult<AstNode> {
+fn var(input: I) -> Res<AstNode> {
     map(ident, AstNode::Var)(input)
 }
 
-fn primary(input: &str) -> PResult<AstNode> {
+fn primary(input: I) -> Res<AstNode> {
     alt((
         lit,
         var,
@@ -40,33 +45,32 @@ fn primary(input: &str) -> PResult<AstNode> {
     ))(input)
 }
 
-fn method_call(input: &str) -> PResult<AstNode> {
-    let (mut rest, mut current) = primary(input)?;
+fn method_call(input: I) -> Res<AstNode> {
+    let (mut i, mut cur) = primary(input)?;
 
-    while let Ok((i2, _)) = delimited(multispace0, tag("."), multispace0)(rest) {
+    while let Ok((i2, _)) = delimited(multispace0, tag("."), multispace0)(i) {
         let (i3, method) = ident(i2)?;
         let (i4, arg) = delimited(
             tag("("),
             delimited(multispace0, expr, multispace0),
             tag(")"),
         )(i3)?;
-        current = AstNode::Call {
-            receiver: Box::new(current),
+        cur = AstNode::Call {
+            receiver: Box::new(cur),
             method,
             args: vec![arg],
             type_args: vec![],
         };
-        rest = i4;
+        i = i4;
     }
-
-    Ok((rest, current))
+    Ok((i, cur))
 }
 
-fn expr(input: &str) -> PResult<AstNode> {
+fn expr(input: I) -> Res<AstNode> {
     method_call(input)
 }
 
-fn let_stmt(input: &str) -> PResult<AstNode> {
+fn let_stmt(input: I) -> Res<AstNode> {
     let (i, _) = preceded(multispace0, tag("let"))(input)?;
     let (i, _) = multispace1(i)?;
     let (i, name) = ident(i)?;
@@ -76,14 +80,14 @@ fn let_stmt(input: &str) -> PResult<AstNode> {
     Ok((i, AstNode::Assign(name, Box::new(e))))
 }
 
-fn stmt(input: &str) -> PResult<AstNode> {
+fn stmt(input: I) -> Res<AstNode> {
     alt((
         let_stmt,
         map(expr, |e| AstNode::Assign("_".to_string(), Box::new(e))),
     ))(input)
 }
 
-fn block(input: &str) -> PResult<Vec<AstNode>> {
+fn block(input: I) -> Res<Vec<AstNode>> {
     delimited(
         delimited(multispace0, tag("{"), multispace0),
         many0(preceded(multispace0, stmt)),
@@ -91,11 +95,11 @@ fn block(input: &str) -> PResult<Vec<AstNode>> {
     )(input)
 }
 
-fn ret_ty(input: &str) -> PResult<String> {
+fn ret_ty(input: I) -> Res<String> {
     preceded(delimited(multispace0, tag("->"), multispace1), ident)(input)
 }
 
-pub fn parse_func(input: &str) -> PResult<AstNode> {
+pub fn parse_func(input: I) -> Res<AstNode> {
     let (i, _) = preceded(multispace0, tag("fn"))(input)?;
     let (i, _) = multispace1(i)?;
     let (i, name) = ident(i)?;
@@ -116,7 +120,7 @@ pub fn parse_func(input: &str) -> PResult<AstNode> {
             name,
             generics: vec![],
             params: vec![],
-            ret: ret.unwrap_or_else(|| "i32".to_string()),
+            ret: ret.unwrap_or("i32".to_string()),
             body,
             where_clause: None,
             attrs: vec![],
@@ -125,6 +129,6 @@ pub fn parse_func(input: &str) -> PResult<AstNode> {
     ))
 }
 
-pub fn parse_zeta(input: &str) -> PResult<Vec<AstNode>> {
+pub fn parse_zeta(input: I) -> Res<Vec<AstNode>> {
     delimited(multispace0, many0(parse_func), multispace0)(input)
 }
