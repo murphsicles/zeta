ba25b7ee632e931422e70a68ae25a2f3fb04b2a7
Roy Murphy <roycoins@gmail.com>
2025-11-21 01:50:42 +0000

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -2,131 +2,114 @@
 use crate::ast::AstNode;
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_while1},
+    bytes::complete::tag,
     character::complete::{alpha1, alphanumeric1, multispace0, multispace1, i64},
-    combinator::{map, opt, recognize},
-    multi::{many0, many1},
-    sequence::{delimited, pair, preceded},
+    combinator::{map, opt},
+    multi::many0,
+    sequence::{delimited, preceded},
     IResult,
 };
 
-fn identifier(input: &str) -> IResult<&str, String> {
-    map(
-        recognize(pair(
-            alt((alpha1, tag("_"))),
-            many0(alt((alphanumeric1, tag("_")))),
-        )),
-        String::from,
-    )(input)
+fn ident(input: &str) -> IResult<&str, String> {
+    let first = alt((alpha1, tag("_")));
+    let rest  = many0(alt((alphanumeric1, tag("_"))));
+    map(nom::sequence::pair(first, rest), |(f, mut r): (&str, Vec<&str>)| {
+        let mut s = f.to_string();
+        for part in r { s.push_str(part); }
+        s
+    })(input)
 }
 
-fn parse_lit(input: &str) -> IResult<&str, AstNode> {
-    map(i64, AstNode::Lit)(input)
-}
+fn lit(input: &str) -> IResult<&str, AstNode> { map(i64, AstNode::Lit)(input) }
+fn var(input: &str) -> IResult<&str, AstNode> { map(ident, AstNode::Var)(input) }
 
-fn parse_var(input: &str) -> IResult<&str, AstNode> {
-    map(identifier, AstNode::Var)(input)
-}
-
-fn parse_primary(input: &str) -> IResult<&str, AstNode> {
+fn primary(input: &str) -> IResult<&str, AstNode> {
     alt((
-        parse_lit,
-        parse_var,
-        delimited(tag("("), parse_expr, tag(")")),
+        lit,
+        var,
+        delimited(tag("("), expr, tag(")")),
     ))(input)
 }
 
-fn parse_method_call(input: &str) -> IResult<&str, AstNode> {
-    let (mut i, mut expr) = parse_primary(input)?;
+fn method_call(input: &str) -> IResult<&str, AstNode> {
+    let (i, recv) = primary(input)?;
+    let mut current = recv;
+    let mut i = i;
 
     while let Ok((i2, _)) = delimited(multispace0, tag("."), multispace0)(i) {
-        let (i3, method) = identifier(i2)?;
+        let (i3, method) = ident(i2)?;
         let (i4, arg) = delimited(
             tag("("),
-            delimited(multispace0, parse_expr, multispace0),
+            delimited(multispace0, expr, multispace0),
             tag(")"),
         )(i3)?;
-        i = i4;
-        expr = AstNode::Call {
-            receiver: format!("{:?}", expr), // temporary – will be proper expr tree later
+        current = AstNode::Call {
+            receiver: "".to_string(),           // will be fixed with proper expr tree later
             method,
-            args: vec![format!("{:?}", arg)],
+            args: vec![format!("{:?}", arg)],  // placeholder – enough to get MIR working
         };
+        i = i4;
     }
-
-    Ok((i, expr))
+    Ok((i, current))
 }
 
-fn parse_expr(input: &str) -> IResult<&str, AstNode> {
-    parse_method_call(input)
-}
+fn expr(input: &str) -> IResult<&str, AstNode> { method_call(input) }
 
-fn parse_let(input: &str) -> IResult<&str, AstNode> {
+fn let_stmt(input: &str) -> IResult<&str, AstNode> {
     let (i, _) = preceded(multispace0, tag("let"))(input)?;
     let (i, _) = multispace1(i)?;
-    let (i, name) = identifier(i)?;
+    let (i, name) = ident(i)?;
     let (i, _) = delimited(multispace0, tag("="), multispace0)(i)?;
-    let (i, expr) = parse_expr(i)?;
+    let (i, e) = expr(i)?;
     let (i, _) = delimited(multispace0, tag(";"), multispace0)(i)?;
-    Ok((i, AstNode::Assign(name, Box::new(expr))))
+    Ok((i, AstNode::Assign(name, Box::new(e))))
 }
 
-fn parse_stmt(input: &str) -> IResult<&str, AstNode> {
+fn stmt(input: &str) -> IResult<&str, AstNode> {
     alt((
-        parse_let,
-        map(parse_expr, |e| {
-            // last expr in block becomes return
-            AstNode::Assign("_".to_string(), Box::new(e))
-        }),
+        let_stmt,
+        map(expr, |e| AstNode::Assign("_".to_string(), Box::new(e))), // last expr becomes return
     ))(input)
 }
 
-fn parse_block(input: &str) -> IResult<&str, Vec<AstNode>> {
+fn block(input: &str) -> IResult<&str, Vec<AstNode>> {
     delimited(
         delimited(multispace0, tag("{"), multispace0),
-        many0(preceded(multispace0, parse_stmt)),
+        many0(preceded(multispace0, stmt)),
         delimited(multispace0, tag("}"), multispace0),
     )(input)
 }
 
-fn parse_return_type(input: &str) -> IResult<&str, String> {
-    preceded(
-        delimited(multispace0, tag("->"), multispace1),
-        identifier,
-    )(input)
+fn ret_ty(input: &str) -> IResult<&str, String> {
+    preceded(delimited(multispace0, tag("->"), multispace1), ident)(input)
 }
 
 pub fn parse_func(input: &str) -> IResult<&str, AstNode> {
     let (i, _) = preceded(multispace0, tag("fn"))(input)?;
     let (i, _) = multispace1(i)?;
-    let (i, name) = identifier(i)?;
+    let (i, name) = ident(i)?;
     let (i, _) = delimited(multispace0, tag("("), multispace0)(i)?;
     let (i, _) = delimited(multispace0, tag(")"), multispace0)(i)?;
-    let (i, ret) = opt(parse_return_type)(i)?;
+    let (i, ret) = opt(ret_ty)(i)?;
     let (i, _) = multispace0(i)?;
-    let (i, body) = parse_block(i)?;
+    let (i, body) = block(i)?;
 
-    let ret_expr = body.iter().rev().find_map(|node| {
-        if let AstNode::Assign(name, expr) = node {
-            if name == "_" { Some(expr.clone()) } else { None }
-        } else {
-            None
-        }
+    // find the last expression (the one assigned to "_") → return expression
+    let ret_expr = body.iter().rev().find_map(|n| match n {
+        AstNode::Assign(s, e) if s == "_" => Some(e.clone()),
+        _ => None,
     });
 
-    Ok((
-        i,
-        AstNode::FuncDef {
-            name,
-            generics: vec![],
-            params: vec![],
-            ret: ret.unwrap_or_else(|| "i32".to_string()),
-            body,
-            where_clause: None,
-            attrs: vec![],
-            ret_expr,
-        },
-    ))
+    Ok((i, AstNode::FuncDef {
+        name,
+        generics: vec![],
+        params: vec![],
+        ret: ret.unwrap_or_else(|| "i32".to_string()),
+        body,
+        where_clause: None,
+        attrs: vec![],
+        ret_expr,
+    }))
 }
 
 pub fn parse_zeta(input: &str) -> IResult<&str, Vec<AstNode>> {
