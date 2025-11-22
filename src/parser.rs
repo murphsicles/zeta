0da37bc71c9cbe7022cadb88a0ab959d7a320d98
Roy Murphy <roycoins@gmail.com>
2025-11-22 07:23:33 +0000

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -7,67 +7,70 @@ use nom::combinator::{map, opt};
 use nom::multi::many0;
 use nom::sequence::{delimited, preceded};
 use nom::IResult;
+use nom::Parser;
 
-fn ws<'a, O>(inner: impl FnMut(&'a str) -> IResult<&'a str, O> + Copy)
-    -> impl FnMut(&'a str) -> IResult<&'a str, O>
-{
+fn ws<'a, O>(inner: impl Parser<&'a str, Output = O, Error = nom::error::Error<&'a str>>) -> impl Parser<&'a str, Output = O, Error = nom::error::Error<&'a str>> {
     delimited(multispace0, inner, multispace0)
 }
 
-fn ident(input: &str) -> IResult<&str, String> {
-    let (i, c) = satisfy(|c| c.is_alphabetic() || c == '_')(input)?;
-    let (i, rest) = many0(satisfy(|c| c.is_alphanumeric() || c == '_'))(i)?;
-    Ok((i, std::iter::once(c).chain(rest).collect()))
+fn ident<'a>() -> impl Parser<&'a str, String, nom::error::Error<&'a str>> {
+    let first = satisfy(|c| c.is_alphabetic() || c == '_');
+    let rest = many0(satisfy(|c| c.is_alphanumeric() || c == '_'));
+    first.and(rest).map(|(c, rest)| std::iter::once(c).chain(rest).collect())
 }
 
-fn literal(input: &str) -> IResult<&str, AstNode> {
-    map(nom_i64, AstNode::Lit)(input)
+fn literal<'a>() -> impl Parser<&'a str, AstNode, nom::error::Error<&'a str>> {
+    map(nom_i64, AstNode::Lit)
 }
 
-fn variable(input: &str) -> IResult<&str, AstNode> {
-    map(ident, AstNode::Var)(input)
+fn variable<'a>() -> impl Parser<&'a str, AstNode, nom::error::Error<&'a str>> {
+    map(ident(), AstNode::Var)
 }
 
-fn expr(input: &str) -> IResult<&str, AstNode> {
-    let (i, left) = alt((literal, variable))(input)?;
-    let plus_parser = ws(tag("+"));
-    match plus_parser(i) {
-        Ok((i, _)) => {
-            let (i, right) = alt((literal, variable))(i)?;
-            Ok((i, AstNode::Call {
-                receiver: Some(Box::new(left)),
-                method: "add".to_string(),
-                args: vec![right],
-                type_args: vec![],
-            }))
-        }
-        Err(nom::Err::Error(_)) => Ok((i, left)),
-        Err(e) => Err(e),
-    }
+fn expr<'a>() -> impl Parser<&'a str, AstNode, nom::error::Error<&'a str>> {
+    let left = alt((literal(), variable()));
+    let op = ws(tag("+"));
+    let right = alt((literal(), variable()));
+    let add = left.clone().and(op).and(right).map(|((left, _), right)| AstNode::Call {
+        receiver: Some(Box::new(left)),
+        method: "add".to_string(),
+        args: vec![right],
+        type_args: vec![],
+    });
+    add.or(left)
 }
 
-fn func_body(input: &str) -> IResult<&str, Vec<AstNode>> {
-    delimited(ws(tag("{")), many0(ws(expr)), ws(tag("}")))(input)
+fn func_body<'a>() -> impl Parser<&'a str, Vec<AstNode>, nom::error::Error<&'a str>> {
+    delimited(ws(tag("{")), many0(ws(expr())), ws(tag("}")))
 }
 
-fn parse_func(input: &str) -> IResult<&str, AstNode> {
-    let (i, _) = ws(tag("fn"))(input)?;
-    let (i, name) = ident(i)?;
-    let (i, _) = ws(tag("("))(i)?;
-    let (i, _) = ws(tag(")"))(i)?;
-    let (i, ret) = opt(preceded(ws(tag("->")), ws(ident)))(i)?;
-    let (i, body) = func_body(i)?;
-    Ok((i, AstNode::FuncDef {
-        name,
-        generics: vec![],
-        params: vec![],
-        ret: ret.unwrap_or_else(|| "i64".to_string()),
-        body,
-        attrs: vec![],
-        ret_expr: None,
-    }))
+fn parse_func<'a>() -> impl Parser<&'a str, AstNode, nom::error::Error<&'a str>> {
+    let fn_kw = ws(tag("fn"));
+    let name = ident();
+    let lparen = ws(tag("("));
+    let rparen = ws(tag(")"));
+    let ret_type = opt(preceded(ws(tag("->")), ws(ident())));
+    let body = func_body();
+    fn_kw
+        .and(name)
+        .and(lparen)
+        .and(rparen)
+        .and(ret_type)
+        .and(body)
+        .map(|((((( _, name), _), _), ret_opt), body)| {
+            let ret = ret_opt.map(ToString::to_string).unwrap_or_else(|| "i64".to_string());
+            AstNode::FuncDef {
+                name,
+                generics: vec![],
+                params: vec![],
+                ret,
+                body,
+                attrs: vec![],
+                ret_expr: None,
+            }
+        })
 }
 
 pub fn parse_zeta(input: &str) -> IResult<&str, Vec<AstNode>> {
-    many0(ws(parse_func))(input)
+    delimited(multispace0, many0(ws(parse_func())), multispace0)(input)
 }
