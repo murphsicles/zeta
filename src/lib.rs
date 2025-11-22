677766b600b58a4ff11ff8fcbaa384bbcab9d764
Roy Murphy <roycoins@gmail.com>
2025-11-22 05:22:07 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+// src/lib.rs
 pub mod ast;
 pub mod borrow;
 pub mod codegen;
 pub mod mir;
 pub mod parser;
 pub mod resolver;
+pub mod specialization;
+pub mod actor;
+pub mod std;
 
+pub use parser::parse_zeta;
+pub use resolver::Resolver;
 pub use codegen::LLVMCodegen;
+pub use actor::{init_runtime, spawn};
