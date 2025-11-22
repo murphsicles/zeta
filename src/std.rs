8dc5abfc0cf9140ea441129e72880c9af5d4128d
Roy Murphy <roycoins@gmail.com>
2025-11-22 05:33:42 +0000

    Update std.rs

--- src/std.rs
+++ src/std.rs
@@ -6,8 +6,7 @@ extern "C" {
     fn free(ptr: *mut c_void);
 }
 
-/// Frees a string returned by http_get/tls_get
-pub unsafe fn free(ptr: *mut u8) {
+pub unsafe fn std_free(ptr: *mut u8) {
     if !ptr.is_null() {
         unsafe { free(ptr as *mut c_void) }
     }
