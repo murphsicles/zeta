707cd96a036f3db2f9b4418d67df8f5ad3a58b87
Roy Murphy <roycoins@gmail.com>
2025-11-23 12:23:59 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,43 @@ pub use parser::parse_zeta;
 pub use resolver::Resolver;
 pub use codegen::LLVMCodegen;
 pub use actor::{init_runtime, spawn};
+
+use inkwell::context::Context;
+use std::error::Error;
+
+pub fn compile_and_run_zeta(code: &str) -> Result<i64, Box<dyn Error>> {
+    actor::init_runtime();
+
+    let (_, asts) = parse_zeta(code).map_err(|e| format!("Parse error: {:?}", e))?;
+
+    let mut resolver = Resolver::new();
+    for ast in &asts {
+        resolver.register(ast.clone());
+    }
+    resolver.typecheck(&asts);
+
+    let context = Context::create();
+    let mut codegen = LLVMCodegen::new(&context, "bench");
+
+    let main_func = asts
+        .iter()
+        .find(|a| matches!(a, AstNode::FuncDef { name, .. } if name == "main"))
+        .ok_or("No main function")?;
+
+    let mir = resolver.lower_to_mir(main_func);
+    codegen.gen_mir(&mir);
+    let ee = codegen.finalize_and_jit()?;
+
+    unsafe {
+        let free_fn = std::std_free as *const () as usize;
+        ee.add_global_mapping(&codegen.module.get_function("free").unwrap(), free_fn);
+    }
+
+    type MainFn = unsafe extern "C" fn() -> i64;
+    unsafe {
+        let main = ee
+            .get_function::<MainFn>("main")
+            .map_err(|_| "No main")?;
+        Ok(main.call())
+    }
+}
