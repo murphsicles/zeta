c16395d820eb1ba92fd612547beddf649a07a103
Roy Murphy <roycoins@gmail.com>
2025-11-23 05:41:57 +0000

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -5,7 +5,7 @@ use nom::bytes::complete::tag;
 use nom::character::complete::{i64 as nom_i64, multispace0, satisfy};
 use nom::combinator::{map, opt};
 use nom::multi::many0;
-use nom::sequence::{delimited, preceded};
+use nom::sequence::{delimited, delimited, preceded};
 use nom::IResult;
 
 fn ws<'a, O>(inner: impl FnMut(&'a str) -> IResult<&'a str, O>) -> impl FnMut(&'a str) -> IResult<&'a str, O> {
@@ -13,9 +13,9 @@ fn ws<'a, O>(inner: impl FnMut(&'a str) -> IResult<&'a str, O>) -> impl FnMut(&'
 }
 
 fn ident(input: &str) -> IResult<&str, String> {
-    let (input, first) = satisfy(|c| c.is_alphabetic() || c == '_')(input)?;
+    let (input, c) = satisfy(|c| c.is_alphabetic() || c == '_')(input)?;
     let (input, rest) = many0(satisfy(|c| c.is_alphanumeric() || c == '_'))(input)?;
-    Ok((input, std::iter::once(first).chain(rest).collect()))
+    Ok((input, std::iter::once(c).chain(rest).collect()))
 }
 
 fn literal(input: &str) -> IResult<&str, AstNode> {
@@ -29,16 +29,22 @@ fn variable(input: &str) -> IResult<&str, AstNode> {
 fn expr(input: &str) -> IResult<&str, AstNode> {
     let (mut input, mut left) = alt((literal, variable))(input)?;
 
-    while let Ok((i, _)) = ws(tag("+"))(input) {
-        input = i;
-        let (i, right) = alt((literal, variable))(input)?;
-        input = i;
-        left = AstNode::Call {
-            receiver: Some(Box::new(left)),
-            method: "add".to_string(),
-            args: vec![right],
-            type_args: vec![],
-        };
+    loop {
+        match ws(tag("+"))(input) {
+            Ok((i, _)) => {
+                input = i;
+                let (i, right) = alt((literal, variable))(input)?;
+                input = i;
+                left = AstNode::Call {
+                    receiver: Some(Box::new(left)),
+                    method: "add".to_string(),
+                    args: vec![right],
+                    type_args: vec![],
+                };
+            }
+            Err(nom::Err::Error(_)) => break,
+            Err(e) => return Err(e),
+        }
     }
 
     Ok((input, left))
@@ -53,14 +59,16 @@ fn parse_func(input: &str) -> IResult<&str, AstNode> {
     let (input, name) = ident(input)?;
     let (input, _) = ws(tag("("))(input)?;
     let (input, _) = ws(tag(")"))(input)?;
-    let (input, ret) = opt(preceded(ws(tag("->")), ws(ident)))(input)?;
+    let (input, ret_opt) = opt(preceded(ws(tag("->")), ws(ident)))(input)?;
     let (input, body) = func_body(input)?;
+
+    let ret = ret_opt.map(|s| s.to_string()).unwrap_or("i64".to_string());
 
     Ok((input, AstNode::FuncDef {
         name,
         generics: vec![],
         params: vec![],
-        ret: ret.map(|s| s.to_string()).unwrap_or("i64".to_string()),
+        ret,
         body,
         attrs: vec![],
         ret_expr: None,
