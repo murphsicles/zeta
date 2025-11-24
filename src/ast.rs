776c6675e814dab849fe74fd53edf4e656838a7c
Roy Murphy <roycoins@gmail.com>
2025-11-24 14:29:19 +0000

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,21 +1,29 @@
 // src/ast.rs
+//! Abstract Syntax Tree for Zeta.
+//! Defines nodes for expressions, statements, definitions, and algebraic constructs.
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum AstNode {
+    /// Full program as list of nodes.
     Program(Vec<AstNode>),
+    /// Trait/concept definition with methods.
     ConceptDef {
         name: String,
         methods: Vec<AstNode>,
     },
+    /// Impl block for concept on type.
     ImplBlock {
         concept: String,
         ty: String,
         body: Vec<AstNode>,
     },
+    /// Method signature in impl/concept.
     Method {
         name: String,
         params: Vec<(String, String)>,
         ret: String,
     },
+    /// Function definition.
     FuncDef {
         name: String,
         generics: Vec<String>,
@@ -25,15 +33,20 @@ pub enum AstNode {
         attrs: Vec<String>,
         ret_expr: Option<Box<AstNode>>,
     },
+    /// Method/trait call.
     Call {
         receiver: Option<Box<AstNode>>,
         method: String,
         args: Vec<AstNode>,
         type_args: Vec<String>,
     },
+    /// Integer literal.
     Lit(i64),
+    /// Variable reference.
     Var(String),
+    /// Assignment.
     Assign(String, Box<AstNode>),
+    /// TimingOwned: Constant-time owned value abstraction.
     TimingOwned {
         ty: String,
         inner: Box<AstNode>,
