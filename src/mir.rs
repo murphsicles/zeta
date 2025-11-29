08c6368e3bded936c3c25bd8462feeaa744d23b8
Roy Murphy <roycoins@gmail.com>
2025-11-29 01:25:00 +0000

    Update mir.rs

--- src/mir.rs
+++ src/mir.rs
@@ -105,13 +105,7 @@ impl MirGen {
                             let mut arg_ids = vec![];
                             for arg in args.iter() {
                                 if let AstNode::Var(ref v) = *arg {
-                                    let id = *self
-                                        .locals
-                                        .entry(v.clone())
-                                        .or_insert_with(|| {
-                                            let id = self.next_id();
-                                            id
-                                        });
+                                    let id = self.lookup_or_alloc(v);
                                     arg_ids.push(id);
                                 }
                             }
@@ -267,7 +261,8 @@ impl MirGen {
     }
 
     fn lookup_or_alloc(&mut self, name: &str) -> u32 {
-        *self.locals.entry(name.to_string()).or_insert_with(|| self.next_id())
+        let key = name.to_string();
+        *self.locals.entry(key).or_insert_with(|| self.next_id())
     }
 
     fn next_id(&mut self) -> u32 {
