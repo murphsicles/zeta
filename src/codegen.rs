e261915d88b2be2dcf99f6e73d5d802b3d69af8f
Roy Murphy <roycoins@gmail.com>
2025-12-03 22:54:06 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -170,10 +170,9 @@ impl<'ctx> LLVMCodegen<'ctx> {
                             });
                             let arg_meta_vals: Vec<BasicMetadataValueEnum<'ctx>> =
                                 arg_vals.iter().map(|v| (*v).into()).collect();
-                            let call_site = self.builder.build_call(callee, &arg_meta_vals, "").unwrap();
-                            let call_res = call_site.try_as_basic_value()
-                                .left()
-                                .unwrap_or_else(|| self.i64_type.const_zero().into());
+                            let call_res = call_site
+                                .try_as_basic_value()
+                                .expect_basic_value_or(self.i64_type.const_zero().into());
                             
                             let ptr = *self.locals.entry(*dest).or_insert_with(|| {
                                 self.builder
