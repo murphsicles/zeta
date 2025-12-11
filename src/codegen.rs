9b7b96c58a265abfd91ecb10408ceb141650a8f6
Roy Murphy <roycoins@gmail.com>
2025-12-11 03:54:50 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -13,7 +13,6 @@ use crate::mir::{Mir, MirExpr, MirStmt, SemiringOp};
 use crate::specialization::{lookup_specialization, record_specialization, MonoKey, MonoValue};
 use crate::xai::XAIClient;
 use inkwell::AddressSpace;
-use inkwell::Either;
 use inkwell::OptimizationLevel;
 use inkwell::attributes::{Attribute, AttributeLoc};
 use inkwell::builder::Builder;
@@ -196,8 +195,8 @@ impl<'ctx> LLVMCodegen<'ctx> {
                                 .build_call(callee, &arg_vals, "call")
                                 .expect("call failed");
 
-                            // Correct way to extract return value from CallSiteValue
-                            if let Either::Left(ret) = call.try_as_basic_value() {
+                            // inkwell 0.7+ returns Either<BasicValueEnum, CallSiteValue>
+                            if let Some(ret) = call.try_as_basic_value().left() {
                                 let ptr = self.locals.entry(*dest).or_insert_with(|| {
                                     self.builder
                                         .build_alloca(self.i64_type, &format!("dest_{}", dest))
