12daae3b1b5d5e0bb90b44292b26fc0881934dc5
Roy Murphy <roycoins@gmail.com>
2025-12-11 03:33:00 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -10,7 +10,7 @@
 
 use crate::actor::{host_channel_recv, host_channel_send, host_spawn};
 use crate::mir::{Mir, MirExpr, MirStmt, SemiringOp};
-use crate::specialization::{MonoKey, lookup_specialization};
+use crate::specialization::{lookup_specialization, record_specialization, MonoKey, MonoValue};
 use crate::xai::XAIClient;
 use inkwell::AddressSpace;
 use inkwell::OptimizationLevel;
@@ -20,8 +20,7 @@ use inkwell::context::Context;
 use inkwell::execution_engine::ExecutionEngine;
 use inkwell::module::{Linkage, Module};
 use inkwell::types::{IntType, PointerType, VectorType};
-use inkwell::values::{BasicValueEnum, PointerValue};
-use inkwell::Either;
+use inkwell::values::{BasicValueEnum, IntValue, PointerValue};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
@@ -147,18 +146,32 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     mono.llvm_func_name
                 } else {
                     let mangled = format!("{}_{}", name, key.type_args.join("_"));
-                    record_specialization(key, MonoValue { llvm_func_name: mangled.clone(), cache_safe: true });
+                    record_specialization(
+                        key,
+                        MonoValue {
+                            llvm_func_name: mangled.clone(),
+                            cache_safe: true,
+                        },
+                    );
                     mangled
                 };
-                let fn_type = self.i64_type.fn_type(&vec![self.i64_type.into(); mir.locals.len()], false);
+                let fn_type = self
+                    .i64_type
+                    .fn_type(&vec![self.i64_type.into(); mir.locals.len()], false);
                 let fn_val = self.module.add_function(&llvm_name, fn_type, None);
                 let entry = self.context.append_basic_block(fn_val, "entry");
                 self.builder.position_at_end(entry);
                 self.locals.clear();
+
+                // Allocate locals
                 for (local_name, &id) in &mir.locals {
-                    let alloca = self.builder.build_alloca(self.i64_type, local_name);
+                    let alloca = self
+                        .builder
+                        .build_alloca(self.i64_type, local_name)
+                        .expect("alloca failed");
                     self.locals.insert(id, alloca);
                 }
+
                 let mut i = 0;
                 while i < mir.stmts.len() {
                     match &mir.stmts[i] {
@@ -168,42 +181,76 @@ impl<'ctx> LLVMCodegen<'ctx> {
                             self.builder.build_store(ptr, val).unwrap();
                         }
                         MirStmt::Call { func, args, dest } => {
-                            let callee = *self.fns.get(func).expect("fn not found");
-                            let arg_vals: Vec<_> = args.iter().map(|&id| self.load_local(id).into()).collect();
-                            let call = self.builder.build_direct_call(callee, &arg_vals, "call").unwrap();
+                            let callee = self
+                                .module
+                                .get_function(func)
+                                .or_else(|| self.fns.get(func).copied())
+                                .expect("function not found");
+                            let arg_vals: Vec<_> = args
+                                .iter()
+                                .map(|&id| self.load_local(id).into())
+                                .collect();
+                            let call = self
+                                .builder
+                                .build_call(callee, &arg_vals, "call")
+                                .expect("call failed");
                             if let Some(ret) = call.try_as_basic_value().left() {
                                 let ptr = self.locals.entry(*dest).or_insert_with(|| {
-                                    self.builder.build_alloca(self.i64_type, &format!("dest_{}", dest))
+                                    self.builder
+                                        .build_alloca(self.i64_type, &format!("dest_{}", dest))
+                                        .expect("alloca failed")
                                 });
                                 self.builder.build_store(*ptr, ret).unwrap();
                             }
                         }
                         MirStmt::VoidCall { func, args } => {
-                            let callee = *self.fns.get(func).expect("fn not found");
-                            let arg_vals: Vec<_> = args.iter().map(|&id| self.load_local(id).into()).collect();
-                            self.builder.build_direct_call(callee, &arg_vals, "call").unwrap();
+                            let callee = self
+                                .module
+                                .get_function(func)
+                                .or_else(|| self.fns.get(func).copied())
+                                .expect("function not found");
+                            let arg_vals: Vec<_> = args
+                                .iter()
+                                .map(|&id| self.load_local(id).into())
+                                .collect();
+                            self.builder
+                                .build_call(callee, &arg_vals, "")
+                                .expect("void call failed");
                         }
                         MirStmt::ParamInit { param_id, arg_index } => {
-                            let param = fn_val.get_nth_param(*arg_index as u32).expect("param not found");
+                            let param = fn_val
+                                .get_nth_param(*arg_index as u32)
+                                .expect("param not found")
+                                .into_int_value();
                             let ptr = *self.locals.get(param_id).expect("local not found");
                             self.builder.build_store(ptr, param).unwrap();
                         }
                         MirStmt::SemiringFold { op, values, result } => {
-                            let mut acc = self.load_local(values[0]);
+                            let mut acc: IntValue<'ctx> = self
+                                .load_local(values[0])
+                                .into_int_value();
                             for &val_id in &values[1..] {
-                                let rhs = self.load_local(val_id);
+                                let rhs = self.load_local(val_id).into_int_value();
                                 acc = match op {
-                                    SemiringOp::Add => self.builder.build_int_add(acc, rhs, "fold_add").unwrap(),
-                                    SemiringOp::Mul => self.builder.build_int_mul(acc, rhs, "fold_mul").unwrap(),
+                                    SemiringOp::Add => self
+                                        .builder
+                                        .build_int_add(acc, rhs, "fold_add")
+                                        .unwrap(),
+                                    SemiringOp::Mul => self
+                                        .builder
+                                        .build_int_mul(acc, rhs, "fold_mul")
+                                        .unwrap(),
                                 };
                             }
                             let ptr = self.locals.entry(*result).or_insert_with(|| {
-                                self.builder.build_alloca(self.i64_type, "fold_res").unwrap()
+                                self.builder
+                                    .build_alloca(self.i64_type, "fold_res")
+                                    .expect("alloca failed")
                             });
                             self.builder.build_store(*ptr, acc).unwrap();
                         }
                         MirStmt::Return { val } => {
-                            let v = self.load_local(*val);
+                            let v = self.load_local(*val).into_int_value();
                             self.builder.build_return(Some(&v)).unwrap();
                         }
                         MirStmt::Consume { id: _ } => {
@@ -212,6 +259,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     }
                     i += 1;
                 }
+
                 self.fns.insert(name.clone(), fn_val);
             }
         }
@@ -239,7 +287,8 @@ impl<'ctx> LLVMCodegen<'ctx> {
                 let global = self.module.add_global(array_ty, None, ".str");
                 global.set_linkage(Linkage::Private);
                 global.set_constant(true);
-                let values: Vec<_> = bytes.iter()
+                let values: Vec<_> = bytes
+                    .iter()
                     .map(|&b| self.context.i8_type().const_int(b as u64, false))
                     .collect();
                 let array_val = self.context.i8_type().const_array(&values);
@@ -256,7 +305,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
         let ptr = *self.locals.get(&id).expect("local not found");
         self.builder
             .build_load(self.i64_type, ptr, &format!("load_{id}"))
-            .unwrap()
+            .expect("load failed")
     }
 
     /// Verifies module, runs MLGO AI passes (vectorize/branch pred), creates JIT engine, maps host functions.
@@ -297,15 +346,38 @@ impl<'ctx> LLVMCodegen<'ctx> {
             }
         }
 
-        let ee = self.module.create_jit_execution_engine(OptimizationLevel::Aggressive)?;
+        let ee = self
+            .module
+            .create_jit_execution_engine(OptimizationLevel::Aggressive)?;
 
-        ee.add_global_mapping(&self.module.get_function("datetime_now").unwrap(), host_datetime_now as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("free").unwrap(), host_free as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("channel_send").unwrap(), host_channel_send as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("channel_recv").unwrap(), host_channel_recv as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("spawn").unwrap(), host_spawn as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("http_get").unwrap(), host_http_get as *const () as usize);
-        ee.add_global_mapping(&self.module.get_function("tls_handshake").unwrap(), host_tls_handshake as *const () as usize);
+        ee.add_global_mapping(
+            &self.module.get_function("datetime_now").unwrap(),
+            host_datetime_now as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("free").unwrap(),
+            host_free as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("channel_send").unwrap(),
+            host_channel_send as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("channel_recv").unwrap(),
+            host_channel_recv as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("spawn").unwrap(),
+            host_spawn as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("http_get").unwrap(),
+            host_http_get as *const () as usize,
+        );
+        ee.add_global_mapping(
+            &self.module.get_function("tls_handshake").unwrap(),
+            host_tls_handshake as *const () as usize,
+        );
 
         Ok(ee)
     }
