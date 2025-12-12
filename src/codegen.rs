f12806aa97a3b0c979f4a56b6b502c4f7abb4078
Roy Murphy <roycoins@gmail.com>
2025-12-12 07:26:25 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -228,12 +228,12 @@ impl<'ctx> LLVMCodegen<'ctx> {
                                 continue;
                             }
                             let lhs_val = self.load_local(values[0]);
-                            let mut acc = lhs_val.into_int_value();
+                            let mut acc = lhs_val.into_int_value().unwrap();
                             for &val_id in &values[1..] {
-                                let val = self.load_local(val_id).into_int_value();
+                                let val = self.load_local(val_id).into_int_value().unwrap();
                                 acc = match op {
-                                    SemiringOp::Add => self.builder.build_int_add(acc, val, "add").unwrap().as_basic_value_enum().into_int_value().unwrap(),
-                                    SemiringOp::Mul => self.builder.build_int_mul(acc, val, "mul").unwrap().as_basic_value_enum().into_int_value().unwrap(),
+                                    SemiringOp::Add => self.builder.build_int_add(acc, val, "add").unwrap(),
+                                    SemiringOp::Mul => self.builder.build_int_mul(acc, val, "mul").unwrap(),
                                 };
                             }
                             if let Some(ptr) = self.locals.get(result) {
