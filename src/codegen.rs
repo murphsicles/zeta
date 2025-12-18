0750ca8714143b220370b27a3de5ead52bbef03a
Roy Murphy <roycoins@gmail.com>
2025-12-18 16:18:26 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -194,7 +194,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     .map(|&id| self.load_local(id).into())
                     .collect();
                 let call = self.builder.build_call(callee_fn, &arg_vals, "call").unwrap();
-                if let Either::Left(basic_val) = call.try_as_basic_value() {
+                if let ValueKind::Basic(basic_val) = call.try_as_basic_value() {
                     let alloca = self.builder.build_alloca(self.i64_type, &format!("call_{dest}")).unwrap();
                     self.builder.build_store(alloca, basic_val).unwrap();
                     self.locals.insert(*dest, alloca);
@@ -288,7 +288,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                         .builder
                         .build_call(concat_fn, &[res.into(), next.into()], "fconcat")
                         .unwrap();
-                    res = if let Either::Left(basic_val) = call.try_as_basic_value() { basic_val } else { panic!("expected basic") };
+                    res = if let ValueKind::Basic(basic_val) = call.try_as_basic_value() { basic_val } else { panic!("expected basic") };
                 }
                 res
             }
