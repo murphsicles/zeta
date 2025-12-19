ff87323ebdfd9c8b0aa0b63dd0f34b5afd9315b4
Roy Murphy <roycoins@gmail.com>
2025-12-19 10:41:50 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -194,12 +194,12 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     .collect();
                 let call = self.builder.build_call(callee_fn, &arg_vals, "call").unwrap();
                 match call.try_as_basic_value() {
-                    inkwell::values::ValueKind::BasicValue(basic_val) => {
+                    inkwell::values::ValueKind::Value(basic_val) => {
                         let alloca = self.builder.build_alloca(self.i64_type, &format!("call_{dest}")).unwrap();
                         self.builder.build_store(alloca, basic_val).unwrap();
                         self.locals.insert(*dest, alloca);
                     }
-                    inkwell::values::ValueKind::Instruction(_) => {
+                    inkwell::values::ValueKind::InstructionValue(_) => {
                         // Instruction with no return value, skip storing
                     }
                 }
@@ -293,8 +293,8 @@ impl<'ctx> LLVMCodegen<'ctx> {
                         .build_call(concat_fn, &[res.into(), next.into()], "fconcat")
                         .unwrap();
                     res = match call.try_as_basic_value() {
-                        inkwell::values::ValueKind::BasicValue(basic_val) => basic_val,
-                        inkwell::values::ValueKind::Instruction(_) => panic!("expected basic value from str_concat"),
+                        inkwell::values::ValueKind::Value(basic_val) => basic_val,
+                        inkwell::values::ValueKind::InstructionValue(_) => panic!("expected basic value from str_concat"),
                     };
                 }
                 res
