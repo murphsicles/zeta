577f67e8e9374c0707a692035d41532bbeb7d76f
Roy Murphy <roycoins@gmail.com>
2025-12-19 10:27:27 +0000

    Update codegen.rs

--- src/codegen.rs
+++ src/codegen.rs
@@ -25,7 +25,7 @@ use inkwell::module::{Linkage, Module};
 use inkwell::support::LLVMString;
 use inkwell::types::{BasicMetadataTypeEnum, IntType, PointerType, VectorType};
 use inkwell::values::{
-    BasicMetadataValueEnum, BasicValue, BasicValueEnum, PointerValue, FunctionValue, ValueKind,
+    BasicMetadataValueEnum, BasicValue, BasicValueEnum, PointerValue, FunctionValue,
 };
 use serde_json::Value;
 use std::collections::HashMap;
@@ -194,7 +194,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
                     .map(|&id| self.load_local(id).into())
                     .collect();
                 let call = self.builder.build_call(callee_fn, &arg_vals, "call").unwrap();
-               if let Either::Left(basic_val) = call.try_as_basic_value() {
+                if let Either::Left(basic_val) = call.try_as_basic_value().left() {
                     let alloca = self.builder.build_alloca(self.i64_type, &format!("call_{dest}")).unwrap();
                     self.builder.build_store(alloca, basic_val).unwrap();
                     self.locals.insert(*dest, alloca);
@@ -288,9 +288,9 @@ impl<'ctx> LLVMCodegen<'ctx> {
                         .builder
                         .build_call(concat_fn, &[res.into(), next.into()], "fconcat")
                         .unwrap();
-                        res = match call.try_as_basic_value() {
-                        Either::Left(basic_val) => basic_val,
-                        Either::Right(_) => panic!("expected basic"),
+                    res = match call.try_as_basic_value().left() {
+                        Some(basic_val) => basic_val,
+                        None => panic!("expected basic value from str_concat"),
                     };
                 }
                 res
