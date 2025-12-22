86781ae57b7c46d6bf0692c02dd072f0bce17ace
Roy Murphy <roycoins@gmail.com>
2025-12-22 07:36:57 +0000

    Update lower.rs

--- src/middle/resolver/lower.rs
+++ src/middle/resolver/lower.rs
@@ -8,49 +8,48 @@ use std::fmt::Debug;
 use super::resolver::Resolver;
 
 impl Resolver {
-    /// Lowers an AST node to MIR.
     pub fn lower_to_mir(&self, ast: &AstNode) -> Mir {
-        // Full lowering logic here; for now, stubbed as in original but ensure buildable
-        // In practice, use MirGen or implement traversal
-        Mir {
-            name: if let AstNode::FuncDef { name, .. } = ast { Some(name.clone()) } else { None },
-            param_indices: vec![],
-            stmts: vec![],
-            exprs: HashMap::new(),
-            ctfe_consts: HashMap::new(),
-        }
+        let mut gen = crate::middle::mir::gen::MirGen::new();
+        gen.lower_to_mir(ast)
     }
 
-    /// Collects used specializations from AST nodes.
     pub fn collect_used_specializations(&self, asts: &[AstNode]) -> HashMap<String, Vec<Vec<String>>> {
         let mut used = HashMap::new();
         for ast in asts {
             if let AstNode::Call { method, type_args, .. } = ast {
-                used.entry(method.clone()).or_insert(vec![]).push(type_args.clone());
+                if !type_args.is_empty() {
+                    used.entry(method.clone()).or_insert(vec![]).push(type_args.clone());
+                }
             }
-            // Recurse into body, etc.
+            // Recurse into bodies if needed
+            if let AstNode::FuncDef { body, .. } = ast {
+                let body_used = self.collect_used_specializations(body);
+                for (fn_name, specs) in body_used {
+                    used.entry(fn_name).or_insert(vec![]).extend(specs);
+                }
+            }
+            // Add for other nodes
         }
         used
     }
 
-    /// Monomorphizes an AST node for a given key.
     pub fn monomorphize(&self, key: MonoKey, ast: &AstNode) -> AstNode {
-        // Full monomorphization: substitute type args
-        // For buildability, clone and modify generics
-        if let AstNode::FuncDef { mut generics, .. } = ast.clone() {
-            generics = vec![]; // Remove generics after mono
-            ast.clone() // Adjusted
+        if let AstNode::FuncDef { generics, .. } = ast {
+            let mut mono_ast = ast.clone();
+            if let AstNode::FuncDef { generics: ref mut g, .. } = mono_ast {
+                *g = vec![];
+            }
+            // Substitute type args in body, params, etc.
+            mono_ast
         } else {
             ast.clone()
         }
     }
 
-    /// Retrieves a cached MIR by AST hash.
     pub fn get_cached_mir(&self, ast_hash: &str) -> Option<Mir> {
         self.cached_mirs.get(ast_hash).cloned()
     }
 
-    /// Retrieves a monomorphized MIR by key.
     pub fn get_mono_mir(&self, key: &MonoKey) -> Option<Mir> {
         self.mono_mirs.get(key).cloned()
     }
