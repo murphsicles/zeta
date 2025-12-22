bb4225f704c9c27ab22cbe3380878f4b2ae00870
Roy Murphy <roycoins@gmail.com>
2025-12-22 08:56:35 +0000

    Update jit.rs

--- src/backend/codegen/jit.rs
+++ src/backend/codegen/jit.rs
@@ -33,7 +33,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
             && let Ok(json) = serde_json::from_str::<Value>(&rec)
             && let Some(passes) = json["passes"].as_array()
         {
-            let fpm: PassManager<FunctionValue<'ctx>> = PassManager::create();
+            let fpm: PassManager<FunctionValue<'ctx>> = PassManager::create(());
             for p in passes {
                 if let Some(ps) = p.as_str() {
                     if ps == "vectorize" {
