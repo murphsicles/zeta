f3881917af4211bfaad2c235a2d02fea8840d103
Roy Murphy <roycoins@gmail.com>
2025-12-22 10:36:04 +0000

    Update ir_gen.rs

--- src/backend/codegen/ir_gen.rs
+++ src/backend/codegen/ir_gen.rs
@@ -2,8 +2,11 @@
 use crate::middle::mir::mir::{Mir, MirExpr, MirStmt, SemiringOp};
 use crate::middle::specialization::{MonoKey, MonoValue, is_cache_safe, lookup_specialization, record_specialization};
 use inkwell::types::BasicMetadataTypeEnum;
-use inkwell::values::{BasicMetadataValueEnum, BasicValueEnum};
+use inkwell::values::{BasicMetadataValueEnum, BasicValueEnum, IntValue, Either};
+use inkwell::module::Linkage;
+use inkwell::values::{FunctionValue, VectorValue};
 use std::collections::HashMap;
+use inkwell::values::BasicValue;
 use super::codegen::LLVMCodegen;
 
 impl<'ctx> LLVMCodegen<'ctx> {
@@ -27,7 +30,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
             let param_val = fn_val.get_nth_param(i as u32).unwrap();
             let alloca = self.builder.build_alloca(self.i64_type, &format!("param_{arg_index}")).unwrap();
             self.builder.build_store(alloca, param_val).unwrap();
-            self.locals.insert(*arg_index as u32, alloca);
+            self.locals.insert(*arg_index, alloca);
         }
         for stmt in &mir.stmts {
             self.gen_stmt(stmt, &mir.exprs);
@@ -38,7 +41,7 @@ impl<'ctx> LLVMCodegen<'ctx> {
     fn gen_stmt(&mut self, stmt: &MirStmt, exprs: &HashMap<u32, MirExpr>) {
         match stmt {
             MirStmt::Assign { lhs, rhs } => {
-                let val = self.gen_expr(rhs, exprs);
+                let val = self.gen_expr(&exprs[rhs], exprs);
                 let alloca = self.builder.build_alloca(self.i64_type, &format!("local_{lhs}")).unwrap();
                 self.builder.build_store(alloca, val).unwrap();
                 self.locals.insert(*lhs, alloca);
@@ -50,91 +53,93 @@ impl<'ctx> LLVMCodegen<'ctx> {
                         val.llvm_func_name
                     } else {
                         let mangled = key.mangle();
-                        let cache_safe = type_args.iter().all(|t| is_cache_safe(t));
-                        record_specialization(key, MonoValue { llvm_func_name: mangled.clone(), cache_safe });
+                        if is_cache_safe(&key) {
+                            record_specialization(key.clone(), MonoValue { llvm_func_name: mangled.clone() });
+                        }
                         mangled
                     }
                 } else {
                     func.clone()
                 };
                 let callee_fn = self.get_callee(&callee);
-                let arg_vals: Vec<BasicMetadataValueEnum> = args.iter().map(|&id| self.load_local(id).into()).collect();
-                let call = self.builder.build_call(callee_fn, &arg_vals, "call").unwrap();
-                if let Some(basic_val) = call.try_as_basic_value().left() {
+                let args_vals: Vec<BasicMetadataValueEnum> = args.iter().map(|&id| self.load_local(id).into()).collect();
+                let call = self.builder.build_call(callee_fn, &args_vals, "call").unwrap();
+                if let Either::Left(basic_val) = call.try_as_basic_value() {
                     let alloca = self.builder.build_alloca(self.i64_type, &format!("call_{dest}")).unwrap();
                     self.builder.build_store(alloca, basic_val).unwrap();
                     self.locals.insert(*dest, alloca);
                 }
             }
             MirStmt::VoidCall { func, args } => {
-                let callee = func.clone();
-                let callee_fn = self.get_callee(&callee);
-                let arg_vals: Vec<BasicMetadataValueEnum> = args.iter().map(|&id| self.load_local(id).into()).collect();
-                self.builder.build_call(callee_fn, &arg_vals, "void_call").unwrap();
+                let callee_fn = self.get_callee(func);
+                let args_vals: Vec<BasicMetadataValueEnum> = args.iter().map(|&id| self.load_local(id).into()).collect();
+                self.builder.build_call(callee_fn, &args_vals, "").unwrap();
             }
             MirStmt::Return { val } => {
                 let ret_val = self.load_local(*val);
                 self.builder.build_return(Some(&ret_val)).unwrap();
             }
-            MirStmt::SemiringFold { op, values, result } => {
-                if values.len() >= 4 {
-                    let mut vec_acc = self.vec4_i64_type.const_zero();
-                    for i in 0..values.len() / 4 {
-                        let vec_vals: Vec<_> = (0..4).map(|j| self.load_local(values[i * 4 + j])).collect();
-                        let mut vec_right = self.vec4_i64_type.const_zero();
-                        vec_right = self.builder.build_insert_element(vec_right, vec_vals[0], 0u64, "").unwrap();
-                        vec_right = self.builder.build_insert_element(vec_right, vec_vals[1], 1u64, "").unwrap();
-                        vec_right = self.builder.build_insert_element(vec_right, vec_vals[2], 2u64, "").unwrap();
-                        vec_right = self.builder.build_insert_element(vec_right, vec_vals[3], 3u64, "").unwrap();
-                        vec_acc = match op {
-                            SemiringOp::Add => self.builder.build_int_add(vec_acc, vec_right, "vec_fold_add").unwrap(),
-                            SemiringOp::Mul => self.builder.build_int_mul(vec_acc, vec_right, "vec_fold_mul").unwrap(),
-                        };
+            MirStmt::SemiringFold { op: SemiringOp::Add, values, result } => {
+                let mut vec_acc: VectorValue<'ctx> = self.vec4_i64_type.const_zero();
+                let chunk_size = 4;
+                for chunk in values.chunks(chunk_size) {
+                    let mut vec_vals: Vec<IntValue<'ctx>> = chunk.iter().map(|&id| self.load_local(id).into_int_value()).collect();
+                    if vec_vals.len() < chunk_size {
+                        vec_vals.resize(chunk_size, self.i64_type.const_zero());
                     }
-                    let mut acc = self.builder.build_extract_element(vec_acc, 0u64, "reduce0").unwrap().into_int_value();
-                    acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, 1u64, "reduce1").unwrap().into_int_value(), "").unwrap();
-                    acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, 2u64, "reduce2").unwrap().into_int_value(), "").unwrap();
-                    acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, 3u64, "reduce3").unwrap().into_int_value(), "").unwrap();
-                    let alloca = self.builder.build_alloca(self.i64_type, &format!("vec_fold_{result}")).unwrap();
-                    self.builder.build_store(alloca, acc).unwrap();
-                    self.locals.insert(*result, alloca);
-                } else {
-                    let mut acc = self.load_local(values[0]);
-                    for &v in &values[1..] {
-                        let right = self.load_local(v);
-                        acc = match op {
-                            SemiringOp::Add => self.builder.build_int_add(acc.into_int_value(), right.into_int_value(), "fold_add").unwrap().into(),
-                            SemiringOp::Mul => self.builder.build_int_mul(acc.into_int_value(), right.into_int_value(), "fold_mul").unwrap().into(),
-                        };
-                    }
-                    let alloca = self.builder.build_alloca(self.i64_type, &format!("fold_{result}")).unwrap();
-                    self.builder.build_store(alloca, acc).unwrap();
-                    self.locals.insert(*result, alloca);
+                    let mut vec_right: VectorValue<'ctx> = self.vec4_i64_type.const_zero();
+                    vec_right = self.builder.build_insert_element(vec_right, vec_vals[0], self.i64_type.const_int(0, false), "").unwrap();
+                    vec_right = self.builder.build_insert_element(vec_right, vec_vals[1], self.i64_type.const_int(1, false), "").unwrap();
+                    vec_right = self.builder.build_insert_element(vec_right, vec_vals[2], self.i64_type.const_int(2, false), "").unwrap();
+                    vec_right = self.builder.build_insert_element(vec_right, vec_vals[3], self.i64_type.const_int(3, false), "").unwrap();
+                    vec_acc = self.builder.build_int_add(vec_acc, vec_right, "vec_add").unwrap();
+                }
+                let mut acc = self.builder.build_extract_element(vec_acc, self.i64_type.const_int(0, false), "reduce0").unwrap().into_int_value();
+                acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, self.i64_type.const_int(1, false), "reduce1").unwrap().into_int_value(), "").unwrap();
+                acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, self.i64_type.const_int(2, false), "reduce2").unwrap().into_int_value(), "").unwrap();
+                acc = self.builder.build_int_add(acc, self.builder.build_extract_element(vec_acc, self.i64_type.const_int(3, false), "reduce3").unwrap().into_int_value(), "").unwrap();
+                let alloca = self.builder.build_alloca(self.i64_type, &format!("vec_fold_{result}")).unwrap();
+                self.builder.build_store(alloca, acc).unwrap();
+                self.locals.insert(*result, alloca);
+            }
+            MirStmt::SemiringFold { op: SemiringOp::Mul, values, result } => {
+                let mut acc = self.i64_type.const_int(1, false);
+                for &val_id in values {
+                    let val = self.load_local(val_id).into_int_value();
+                    acc = self.builder.build_int_mul(acc, val, "mul").unwrap();
+                }
+                let alloca = self.builder.build_alloca(self.i64_type, &format!("fold_{result}")).unwrap();
+                self.builder.build_store(alloca, acc).unwrap();
+                self.locals.insert(*result, alloca);
+            }
+            MirStmt::ParamInit { param_id, arg_index } => {
+                let param_val = self.builder.build_alloca(self.i64_type, &format!("init_param_{param_id}")).unwrap();
+                let arg_val = self.i64_type.const_int(*arg_index as u64, false);
+                self.builder.build_store(param_val, arg_val).unwrap();
+                self.locals.insert(*param_id, param_val);
+            }
+            MirStmt::Consume { id } => {
+                let ptr = self.locals.remove(id);
+                if let Some(p) = ptr {
+                    self.builder.build_free(p).unwrap();
                 }
             }
-            MirStmt::ParamInit { .. } => {}
-            MirStmt::Consume { .. } => {}
             MirStmt::If { cond, then, else_ } => {
-                let cond_val = self.load_local(*cond);
-                let parent = self.builder.get_insert_block().unwrap().get_parent().unwrap();
-                let then_bb = self.context.append_basic_block(parent, "then");
-                let else_bb = self.context.append_basic_block(parent, "else");
-                let merge_bb = self.context.append_basic_block(parent, "merge");
-                self.builder.build_conditional_branch(cond_val.into_int_value(), then_bb, else_bb).unwrap();
+                let cond_val = self.gen_expr(&exprs[cond], exprs).into_int_value();
+                let then_bb = self.context.append_basic_block(self.builder.get_insert_block().unwrap().get_parent().unwrap(), "then");
+                let else_bb = self.context.append_basic_block(self.builder.get_insert_block().unwrap().get_parent().unwrap(), "else");
+                let merge_bb = self.context.append_basic_block(self.builder.get_insert_block().unwrap().get_parent().unwrap(), "merge");
+                self.builder.build_conditional_branch(cond_val, then_bb, else_bb).unwrap();
                 self.builder.position_at_end(then_bb);
                 for stmt in then {
                     self.gen_stmt(stmt, exprs);
                 }
-                if self.builder.get_insert_block().unwrap().get_terminator().is_none() {
-                    self.builder.build_unconditional_branch(merge_bb).unwrap();
-                }
+                self.builder.build_unconditional_branch(merge_bb).unwrap();
                 self.builder.position_at_end(else_bb);
                 for stmt in else_ {
                     self.gen_stmt(stmt, exprs);
                 }
-                if self.builder.get_insert_block().unwrap().get_terminator().is_none() {
-                    self.builder.build_unconditional_branch(merge_bb).unwrap();
-                }
+                self.builder.build_unconditional_branch(merge_bb).unwrap();
                 self.builder.position_at_end(merge_bb);
             }
         }
@@ -159,12 +164,12 @@ impl<'ctx> LLVMCodegen<'ctx> {
                 if ids.is_empty() {
                     return self.i64_type.const_int(0, false).into();
                 }
-                let mut res = self.gen_expr(&MirExpr::Var(ids[0]), _exprs);
+                let mut res = self.gen_expr(&exprs[&ids[0]], _exprs);
                 for &id in &ids[1..] {
-                    let next = self.gen_expr(&MirExpr::Var(id), _exprs);
+                    let next = self.gen_expr(&exprs[&id], _exprs);
                     let concat_fn = self.get_callee("str_concat");
                     let call = self.builder.build_call(concat_fn, &[res.into(), next.into()], "fconcat").unwrap();
-                    if let Some(basic_val) = call.try_as_basic_value().left() {
+                    if let Either::Left(basic_val) = call.try_as_basic_value() {
                         res = basic_val;
                     }
                 }
@@ -175,7 +180,8 @@ impl<'ctx> LLVMCodegen<'ctx> {
                 let ptr = self.locals[inner_id];
                 let load = self.builder.build_load(self.i64_type, ptr, "timing_load").unwrap();
                 if let Some(inst) = load.as_instruction_value() {
-                    inst.set_metadata(self.tbaa_const_time, 0).unwrap();
+                    let tbaa_kind = self.context.get_kind_id("tbaa");
+                    inst.set_metadata(self.tbaa_const_time, tbaa_kind).unwrap();
                 }
                 load
             }
