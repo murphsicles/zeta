55ff6e0d75c00b10cdbbdaa7500765c343bc1791
Roy Murphy <roycoins@gmail.com>
2025-12-22 09:09:52 +0000

    Update mod.rs

--- src/runtime/actor/mod.rs
+++ src/runtime/actor/mod.rs
@@ -1,5 +1,9 @@
 // src/runtime/actor/mod.rs
 pub mod channel;
 pub mod scheduler;
-pub use channel::{host_channel_recv, host_channel_send, Channel};
+pub mod result;
+pub mod map;
+pub use channel::{Channel, host_channel_recv, host_channel_send};
 pub use scheduler::{host_spawn, init_runtime, spawn};
+pub use result::{host_result_free, host_result_get_data, host_result_is_ok, host_result_make_err, host_result_make_ok};
+pub use map::{host_map_free, host_map_get, host_map_insert, host_map_new};
