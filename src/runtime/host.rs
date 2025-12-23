2397265e1796889210b41d3f606f06201eabf930
Roy Murphy <roycoins@gmail.com>
2025-12-23 17:20:23 +0000

    Update host.rs

--- src/runtime/host.rs
+++ src/runtime/host.rs
@@ -12,12 +12,14 @@ pub unsafe extern "C" fn host_datetime_now() -> i64 {
         .as_millis() as i64
 }
 
+#[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn host_free(ptr: *mut c_void) {
     if !ptr.is_null() {
         free(ptr);
     }
 }
 
+#[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn host_http_get(url: *const c_char) -> i64 {
     if let Ok(url_str) = unsafe { CStr::from_ptr(url) }.to_str() {
         // Dummy impl: assume success.
@@ -27,6 +29,7 @@ pub unsafe extern "C" fn host_http_get(url: *const c_char) -> i64 {
     }
 }
 
+#[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn host_tls_handshake(host: *const c_char) -> i64 {
     if unsafe { CStr::from_ptr(host) }.to_str().is_ok() {
         0i64
@@ -35,6 +38,7 @@ pub unsafe extern "C" fn host_tls_handshake(host: *const c_char) -> i64 {
     }
 }
 
+#[allow(unsafe_op_in_unsafe_fn)]
 pub unsafe extern "C" fn host_str_concat(a: *const u8, a_len: usize, b: *const u8, b_len: usize) -> *mut u8 {
     let mut result = Vec::with_capacity(a_len + b_len + 1);
     result.extend_from_slice(unsafe { std::slice::from_raw_parts(a, a_len) });
