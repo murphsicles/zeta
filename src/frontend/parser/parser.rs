ddcf10cd64ae06640f0b66c6a7a358e0344a7abc
Roy Murphy <roycoins@gmail.com>
2025-12-24 06:51:14 +0000

    Update parser.rs

--- src/frontend/parser/parser.rs
+++ src/frontend/parser/parser.rs
@@ -11,9 +11,9 @@ use nom::branch::alt;
 #[allow(unused_imports)]
 use nom::combinator::{map, opt};
 
-pub fn ws<'a, F, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, nom::error::Error<&'a str>>
+pub fn ws<'a, F, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O>
 where
-    F: FnMut(&'a str) -> IResult<&'a str, O, nom::error::Error<&'a str>>,
+    F: FnMut(&'a str) -> IResult<&'a str, O>,
 {
     move |input| {
         let (input, _) = multispace0(input)?;
@@ -23,18 +23,18 @@ where
     }
 }
 
-pub fn parse_ident(input: &str) -> IResult<&str, String, nom::error::Error<&str>> {
+pub fn parse_ident(input: &str) -> IResult<&str, String> {
     map(pair(alpha1, alphanumeric0), |(first, rest): (&str, &str)| first.to_string() + rest)(input)
 }
 
-pub fn parse_keyword(kw: &'static str) -> impl FnMut(&str) -> IResult<&str, (), nom::error::Error<&str>> {
+pub fn parse_keyword(kw: &'static str) -> impl FnMut(&str) -> IResult<&str, ()> {
     value((), ws(tag(kw)))
 }
 
-pub fn parse_path(input: &str) -> IResult<&str, Vec<String>, nom::error::Error<&str>> {
+pub fn parse_path(input: &str) -> IResult<&str, Vec<String>> {
     many1(preceded(opt(tag("::")), parse_ident))(input)
 }
 
-pub fn parse_generics(input: &str) -> IResult<&str, Vec<String>, nom::error::Error<&str>> {
+pub fn parse_generics(input: &str) -> IResult<&str, Vec<String>> {
     delimited(tag("<"), separated_list1(tag(","), parse_ident), tag(">"))(input)
 }
