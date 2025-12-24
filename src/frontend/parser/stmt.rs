57b73e3f028f66f78ab5fd0fbd0698afc0e89bae
Roy Murphy <roycoins@gmail.com>
2025-12-24 06:53:43 +0000

    Update stmt.rs

--- src/frontend/parser/stmt.rs
+++ src/frontend/parser/stmt.rs
@@ -10,20 +10,20 @@ use nom::{IResult};
 use super::parser::ws;
 use super::expr::parse_full_expr;
 
-pub fn parse_assign(input: &str) -> IResult<&str, AstNode, nom::error::Error<&str>> {
+pub fn parse_assign(input: &str) -> IResult<&str, AstNode> {
     let (input, lhs) = ws(parse_full_expr)(input)?;
     let (input, _) = ws(tag("="))(input)?;
     let (input, rhs) = ws(parse_full_expr)(input)?;
     Ok((input, AstNode::Assign(Box::new(lhs), Box::new(rhs))))
 }
 
-pub fn parse_return(input: &str) -> IResult<&str, AstNode, nom::error::Error<&str>> {
+pub fn parse_return(input: &str) -> IResult<&str, AstNode> {
     let (input, _) = ws(tag("return"))(input)?;
     let (input, inner) = ws(parse_full_expr)(input)?;
     Ok((input, AstNode::Return(Box::new(inner))))
 }
 
-pub fn parse_if(input: &str) -> IResult<&str, AstNode, nom::error::Error<&str>> {
+pub fn parse_if(input: &str) -> IResult<&str, AstNode> {
     let (input, _) = ws(tag("if"))(input)?;
     let (input, cond) = ws(parse_full_expr)(input)?;
     let (input, then) = delimited(ws(tag("{")), many0(ws(parse_stmt)), ws(tag("}")))(input)?;
@@ -32,6 +32,6 @@ pub fn parse_if(input: &str) -> IResult<&str, AstNode, nom::error::Error<&str>> 
     Ok((input, AstNode::If { cond: Box::new(cond), then, else_ }))
 }
 
-pub fn parse_stmt(input: &str) -> IResult<&str, AstNode, nom::error::Error<&str>> {
+pub fn parse_stmt(input: &str) -> IResult<&str, AstNode> {
     alt((parse_assign, parse_return, parse_if, parse_full_expr))(input)
 }
