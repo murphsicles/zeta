48fe47c0a2b62a7cfbdd7535274d7f026e050e38
Roy Murphy <roycoins@gmail.com>
2025-12-25 15:41:49 +0000

    Update typecheck.rs

--- src/middle/resolver/typecheck.rs
+++ src/middle/resolver/typecheck.rs
@@ -4,9 +4,10 @@ use super::resolver::{Resolver, Type};
 
 impl Resolver {
     pub fn typecheck(&mut self, asts: &[AstNode]) -> bool {
+        let mut borrow_checker = &mut self.borrow_checker;
         let resolver = &*self;
         for ast in asts {
-            if !self.borrow_checker.check(ast, resolver) {
+            if !borrow_checker.check(ast, resolver) {
                 return false;
             }
         }
